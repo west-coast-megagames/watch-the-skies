f7f09f74f65e6420010eefbdbfad722a670530c3
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2019-10-10 14:23:05 -0700

    Cleaned Intercept Algorithm.

--- server/util/systems/intercept/intercept.js
+++ server/util/systems/intercept/intercept.js
@@ -1,9 +1,7 @@
-const test = require('./test');
 const atkOutcome = require('./attacker');
 const defOutcome = require('./defender');
 const interceptDmg = require('./damage');
 
-// intercept(test.attacker, test.defender); // Test call of Interception Algorithm
 
 // Interception Algorithm - Expects an attacker object and a defender object from MongoDB
 function intercept (attacker, defender) {
@@ -24,15 +22,6 @@ function intercept (attacker, defender) {
     console.log(result.defenderReport);
 
     return finalReport;
-};
-
-// Fake interception algorithm to test DB input to intercept system
-function interceptTest (attacker, defender) {
-    // console.log(attacker)
-    let atkStatus = "testing"
-    console.log(`${attacker.designation} confirmed as attacker!`);
-    console.log(`${defender.designation} confirmed as defender!`);
-    console.log(`${attacker.designation} is on an ${atkStatus} mission...`);
 };
 
 module.exports = intercept;
