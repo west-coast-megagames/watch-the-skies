cde6f85a7dcdfb44530a39491af4dc800db36311
jeffwqc <jeffwqc@gmail.com>
2019-10-16 22:57:54 -0700

    Changed intercept from post to put for update

--- server/routes/api/intercept.js
+++ server/routes/api/intercept.js
@@ -5,17 +5,17 @@ const intercept = require('../../util/systems/intercept/intercept');
 // Item Model
 const Interceptor = require('../../models/interceptor');
 
-// @route   GET api/intercept
+// @route   PUT api/intercept   ... update
 // @Desc    Find Attacker/Defender and activate intercept
 // @access  Public
-router.post('/', async function(req, res) {
+router.put('/', async function(req, res) {
     console.log(req.body);
     let atkId = req.body.attacker;
     console.log(atkId);
     let defId = req.body.defender;
     console.log(defId);
-    let attacker = await Interceptor.findById(atkId)
-    let defender = await Interceptor.findById(defId)
+    let attacker = await Interceptor.findById(atkId);
+    let defender = await Interceptor.findById(defId);
     result = intercept(attacker, defender);
     res.json(result);
 });
