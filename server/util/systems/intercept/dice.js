d1ad55eb67459dd7e9f50fae7ec6b4ce8667f137
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2019-10-18 16:42:19 -0700

    Added more dice types for random rolls.

--- server/util/systems/intercept/dice.js
+++ server/util/systems/intercept/dice.js
@@ -1,7 +1,33 @@
-// Random d6 die roll
+// Random die rolls - Exports d4, d6, d8, d10, d12, and d20.
+
+function d4() {
+    let rand = 1 + Math.floor(Math.random() * 4);
+    return rand;
+}
+
 function d6() {
     let rand = 1 + Math.floor(Math.random() * 6);
     return rand;
 };
 
-module.exports = d6;
+function d8() {
+    let rand = 1 + Math.floor(Math.random() * 8);
+    return rand;
+};
+
+function d10() {
+    let rand = 1 + Math.floor(Math.random() * 10);
+    return rand;
+};
+
+function d12() {
+    let rand = 1 + Math.floor(Math.random() * 12);
+    return rand;
+};
+
+function d20() {
+    let rand = 1 + Math.floor(Math.random() * 20);
+    return rand;
+};
+
+module.exports = { d4, d6, d8, d10, d12, d20 };
