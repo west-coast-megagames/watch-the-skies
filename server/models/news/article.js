c8f33eee9f6dda541016afc4fbe80cea9ea72fe7
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2019-11-03 14:33:25 -0800

    Added Article Model

--- server/models/news/article.js
+++ server/models/news/article.js
@@ -4,8 +4,11 @@ const Joi = require('joi');
 
 const ArticleSchema = new Schema({
   agency: { type: String, uppercase: true, required: true },
-  turn: { type: String, required: true },
-  date: { type: Date, default: Date.now() },
+  timestamp: {
+    date: { type: Date, default: Date.now() },
+    turn: { type: String, default: "Test Turn"},
+    phase: { type: String, default: "Test Phase"},
+  },
   location: { type: String, required: true, minlength: 2, maxlength: 2 },
   headline: { type: String, required: true, minlength: 10, maxlength: 100 },
   body: {type: String, required: true, minlength: 60, maxlength: 1000},
@@ -17,8 +20,7 @@ let Article = mongoose.model('article', ArticleSchema);
 function validateArticle(article) {
   const schema = {
     agency: Joi.string().required(),
-    turn: Joi.string().required(),
-    date: Joi.date(), 
+    //turn: Joi.string().required(),
     location: Joi.string().min(2).max(2).required(),
     headline: Joi.string().min(10).max(100).required(),
     body: Joi.string().min(60).max(1000).required(), 
