b39d17a6d52cce4cad2e4091a48477a952043f0a
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2019-12-31 16:31:08 -0800

    MINOR - Change to boot order

--- server/server.js
+++ server/server.js
@@ -8,23 +8,20 @@ require('supports-color');
 const { alerts } = require('./wts/notifications/alerts');
 
 // Start up proceesses
-const app = express();
+const app = express(); // Init for express
+const server = http.createServer(app); // Creation of an HTTP server
+const io = require('socket.io')(server); // Creation of websocket Server
+
 require('./startup/logging')(); // Bootup for error handling
 require('./startup/routes')(app); // Bootup for Express routes
 require('./startup/db')(); // Bootup of MongoDB through Mongoose
 require('./startup/config')(); // Bootup for special configurations
 require('./startup/prod')(app); // Production compression and middleware
-
-const server = http.createServer(app); // Creation of an HTTP server
-const io = require('socket.io')(server); // Creation of websocket Server
-
-// Socket.io routes (Currently housed in config/sockets.js)
 require('./config/sockets')(io); // Main websocket routes
+// Socket.io routes are currently housed in config/sockets.js)
 alerts(io); // Temp alert route for v0.1.1
 
-// Server entry point - Node Server
-const port = process.env.PORT || 5000;
-//export server object for integration testing
-server.listen(port, () => logger.info(`WTS Server started on port ${port}...`));
+const port = process.env.PORT || 5000; // Server entry point - Node Server
+server.listen(port, () => logger.info(`WTS Server started on port ${port}...`)); //export server object for integration testing
 
 module.exports = server;
