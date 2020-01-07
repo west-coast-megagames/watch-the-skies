ae30156a864d7519c7aa43663a72f8d997459d9b
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-01-07 04:38:10 -0800

    ADDED - Chat page using rsuite

---
+++ reactapp/src/pages/chat.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'; // React import
+import { Container, Content, Sidebar } from 'rsuite';
+
+class Chat extends Component {
+    constructor() {
+        super();
+        this.state = {
+          tab: 'dashboard'
+        };
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    getActive(element) {
+        return element === this.state.tab ? '' : 'hidden'
+    }
+
+    handleSelect(activeKey) {
+        this.setState({ tab: activeKey })
+    }
+
+    render() {
+        const { tab } = this.state; 
+
+         return (
+        <Container>
+            <Sidebar style={{ paddingLeft: 20 }}>
+            </Sidebar>
+            <Content>
+                <h5>The chat system has not been created!</h5>
+            </Content>
+        </Container>
+         );
+     }
+ }
+
+export default Chat;
