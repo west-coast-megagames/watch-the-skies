67e7d8c3360ec11c7e47d6888a8d6bd3fe0a00fe
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-01-15 03:26:30 -0800

    MINOR - v0.1.2 fixes

--- reactapp/src/pages/home.jsx
+++ reactapp/src/pages/home.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const Home = (props) => {
     return (
         <div className="center-text">
-            <h4>Current Build v0.1.1 - Budget and Interceptions</h4>
-            <p>This is a test application for Watch the Skies by West Coast Megagames!</p>
+            <h4>Current Build v0.1.2 - Multi-user login and Authentication</h4>
+            <p>This is a test application for Watch the Skies created by the Project Nexus team of West Coast Megagames!</p>
+            <p> We are currently trying to build out digital applications to be the backbone of our megagame events.</p>
             {props.login === true && 
                 <p>Goodluck testing out the application - you will need a control member to start the clock for you if you don't know how to!</p>}
             {props.login === false &&
