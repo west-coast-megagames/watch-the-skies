654fcc1589e94c967cd375da3763401aa9f97e85
Shawn Pearson <shawmn.m.pearson@gmail.com>
2020-01-18 13:47:36 -0800

    Fix for bcrypt error...now using bcryptjs

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const auth = require('../middleware/auth');
 const validateObjectId = require('../middleware/validateObjectId');
 
