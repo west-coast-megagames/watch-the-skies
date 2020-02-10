620d756f460eaf995ee6f6b68953d5de385196af
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-02-10 11:24:47 -0800

    ADDED - Scrpts folder for utility functions

---
+++ reactapp/src/scripts/range.js
@@ -0,0 +1,12 @@
+// Function to get the distance between two lat/lon points using the ‘haversine’ formula
+function distance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+            c(lat1 * p) * c(lat2 * p) * 
+            (1 - c((lon2 - lon1) * p))/2;
+  
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
+export default distance;
