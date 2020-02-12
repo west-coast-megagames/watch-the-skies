5a3befb09ad5b4fb0fd0d96e47cafd25f04cd555
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-02-11 16:57:58 -0800

    MINOR - Property threading simplification

--- reactapp/src/pages/tabs/gov/budget.jsx
+++ reactapp/src/pages/tabs/gov/budget.jsx
@@ -1,40 +1,34 @@
-import React from 'react'; // React import
+import React, {Component} from 'react'; // React import
 import TransferForm from '../../../components/transferForm';
 import ChartsPage from '../../../components/graph';
 import AccountsTable from '../../../components/accountsTable'
 import AutoTransfers from '../../../components/transfersTable';
 import { Container, Header, Content, Footer, Sidebar } from 'rsuite';
 
-const Budget = (props) => {
-    return (
-        <Container className="budget-tab">
-            <Header className="transfers">
-                <TransferForm
-                    team={ props.team }
-                    accounts={ props.accounts }
-                    handleUpdate={ props.handleUpdate }
-                    alert={ props.alert }
-                />
-            </Header>
-            <Container className="transfers">
-                <Sidebar>
-                    <AccountsTable accounts={ props.accounts } />
-                </Sidebar>
-                <Content>
-                    <ChartsPage
-                        team ={ props.team }
-                        accounts={ props.accounts }
+class Budget extends Component {
+    state = { account: {}}
+    render() { 
+        return (
+            <Container className="budget-tab">
+                <Header className="transfers">
+                    <TransferForm {...this.props}
                     />
-                </Content>
+                </Header>
+                <Container className="transfers">
+                    <Sidebar>
+                        <AccountsTable accounts={ this.props.accounts } />
+                    </Sidebar>
+                    <Content>
+                        <ChartsPage {...this.props} />
+                    </Content>
+                </Container>
+                <Footer>
+                    <h4>Automatic Transfers</h4>
+                    <AutoTransfers accounts={ this.props.accounts } />
+                </Footer>
             </Container>
-            <Footer>
-                <h4>Automatic Transfers</h4>
-                <AutoTransfers
-                        accounts={ props.accounts }
-                />
-            </Footer>
-        </Container>
-    );
+        );
+    }
 }
  
 export default Budget;
