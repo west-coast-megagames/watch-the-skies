0d425f001434429faed47e16b8a469d3ac0e0eed
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-02-13 21:03:26 -0800

    ADDED - Equipment base model and discriminators

---
+++ server/models/gov/equipment/systems.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { Equipment } = require('./equipment');
+const Schema = mongoose.Schema;
+const Joi = require('joi');
+
+const System = Equipment.discriminator('System', new Schema({
+    type: { type: String, default: 'System' },
+    category: { type: String, enum: [ 'Weapon', 'Engine', 'Sensor', 'Compartment', 'Util', 'Chassis' ] },
+    stats: {
+        hullMax: { type: Number },
+        attack: { type: Number },
+        penetration: { type: Number },
+        armor: { type: Number },
+        sheild: { type: Number },
+        evade: { type: Number },
+        range: { type: Number },
+        cargo: { type: Number }
+    }
+}));
+
+module.exports = { System };
