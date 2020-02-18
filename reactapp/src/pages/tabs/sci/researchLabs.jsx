c962af89efac470662f40851a9673c527ca9410c
drewwww <drewwwwdotcom@yahoo.com>
2020-02-17 20:26:29 -0800

    updated select picker to prelabel the research

--- reactapp/src/pages/tabs/sci/researchLabs.jsx
+++ reactapp/src/pages/tabs/sci/researchLabs.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Progress, Table, InputNumber, Input } from 'rsuite';
-import MySelectPicker from './myselectpicker';
+import { Progress, Table, InputNumber, Tag, SelectPicker } from 'rsuite';
 
 const { Column, HeaderCell, Cell } = Table;
 
@@ -23,10 +22,9 @@ const ProgressCell = ({ rowData, dataKey, ...props }) => {
 		let myTechCost = 1;		// Tech Cost for 100% completion of myResearch 	
 		const result = newLabCheck(rowData._id, props.labupdates);
 		if (result >= 0) {		// Lab was updated, so find the new %
-			let myID = props.labupdates[result].research;	// ID of the tech being researched in this row
-			myResearch = props.allknowledge.filter(el => el._id === myID);
-
-			if (myID !== null) {	// Most cases, obj is a number.  When removed via "X", it becomes null
+			let myResearchID = props.labupdates[result].research_id;	// ID of the tech being researched in this row
+			if (myResearchID !== null) {	// Most cases, obj is a number.  When removed via "X", it becomes null
+				myResearch = props.allknowledge.filter(el => el._id === myResearchID);
 				myProgress = myResearch[0].status.progress;
 				myLevel    = myResearch[0].level;
 				myTechCost = props.techcost[myLevel];
@@ -59,18 +57,7 @@ const ProgressCell = ({ rowData, dataKey, ...props }) => {
 	}
 };
 
-const InputNumberCell = ({ rowData, dataKey, ...props }) => {
-	function updateFunding () {
-		//props.alert({type: 'success', title: 'Updating Funding', body: `${rowData._id} is working on ${rowData.name}`})
-		return (4);
-	}
-	const drewjunk = updateFunding();
-	return (
-	<Cell {...props} style={{ padding: 0 }} dataKey="blah" >
-		<InputNumber prefix="Funding" max={drewjunk} min={0} step={1} style={{ width: 140 }}/>
-	</Cell>
-	);
-};
+
 
 class ResearchLabs extends Component {
 	constructor() {
@@ -80,33 +67,51 @@ class ResearchLabs extends Component {
 			labUpdates: [],
 			labs : []
 		}
-		this.handleUpdate = this.handleUpdate.bind(this);
+		this.handleLabUpdate = this.handleLabUpdate.bind(this);
+		this.handleFundingUpdate = this.handleFundingUpdate.bind(this);
 	}
 	
-	handleUpdate(updatedLab) {
+	handleLabUpdate(updatedLab) {
 		let labUpdates = this.state.labUpdates;
 		const result = newLabCheck(updatedLab._id, labUpdates);
 		if (result === -1) {				// New Entry
-				labUpdates.push(updatedLab);
+			labUpdates.push(updatedLab);
 		} else {							// Existing Entry
-			labUpdates[result].research = updatedLab.research;
+			labUpdates[result].research_id = updatedLab.research_id;
 		}
 		this.setState({labUpdates});
-		//this.props.alert({type: 'success', title: 'Research Selected', body: `${updatedLab.lab} is working on ${updatedLab.research}`})
+		//this.props.alert({type: 'success', title: 'Research Selected', body: `${updatedLab.lab} is working on ${updatedLab.research_id}`})
+	}
+  
+	handleFundingUpdate(updatedLab) {
+		let labUpdates = this.state.labUpdates;
+		const result = newLabCheck(updatedLab._id, labUpdates);
+		if (result === -1) {				// New Entry
+			updatedLab.research_id = null;		
+			labUpdates.push(updatedLab);  
+		} else {							// Existing Entry
+			labUpdates[result].funding = updatedLab.funding;
+		}
+		this.setState({labUpdates});
+		//this.props.alert({type: 'success', title: 'Research Selected', body: `${updatedLab.lab} is working on ${updatedLab.research_id}`})
 	}
   
 	componentDidMount(){
 		let research = this.props.allKnowledge.filter(el => el.type !== "Knowledge" && el.team === this.props.team._id);
 		let labs = this.props.facilities.filter(el => el.type === 'Lab' && el.team._id === this.props.team._id);
+		
+		labs.forEach(el => { 
+			el.disableFunding = false;
+		});
+
         this.setState({research, labs});
 	}
 	
 	render() { 
-		//console.log('TEAM=', this.props.team);
-		//console.log('EVERYTHING=', this.props.everythingProp);
 		let props = this.props;
 		let research = this.state.research;
-		let sendUpdate = this.handleUpdate;
+		let sendLabUpdate = this.handleLabUpdate;
+		let sendFundingUpdate = this.handleFundingUpdate;
 		return(
 			<div>
 				<Table
@@ -119,24 +124,29 @@ class ResearchLabs extends Component {
                     <Cell dataKey="name" />
                 </Column>
         
-                <Column verticalAlign='middle' width={250} align="left" fixed>
+                <Column verticalAlign='top' width={250} align="left" fixed>
                     <HeaderCell>Action</HeaderCell>
                     <Cell dataKey="name">
-					{rowData => {      
+					{rowData => {   
 						function handleChange(value) {
-							let updatedLab = { _id: rowData._id, research: value };
-							sendUpdate(updatedLab);
+							let updatedLab = { 
+								_id: rowData._id, 
+								research_id: value,
+								//funding : rowData.funding
+								funding: null 
+							};
+							sendLabUpdate(updatedLab);
 						}          
 						return (
-							<MySelectPicker
-								lab={ rowData.lab }
-								team={ props.team }
-								allKnowledge={ props.allKnowledge }
-								availibleResearch={ research }
-								//accounts={ this.props.accounts }
-								handleChange={ handleChange }
-								alert={ props.alert }
-							/>   
+							<SelectPicker
+								defaultValue={ rowData.research[0] }
+								groupBy='field'
+								valueKey='_id'
+								labelKey='name'
+								onChange={handleChange}
+								data={ research }
+								style={{ width: 224 }}
+							/>
 						)}}
 					</Cell>
                 </Column>
@@ -151,21 +161,52 @@ class ResearchLabs extends Component {
 					/>
                 </Column>
         
-                <Column verticalAlign='middle' width={230}>
+                <Column verticalAlign='middle' width={150}>
                     <HeaderCell>Funding Level</HeaderCell>
-					<InputNumberCell 
-						prefix="Funding" 
-						max={4} 
-						min={0} 
-						step={1} 
-						style={{ width: 140 }}
-					/>
+                    <Cell style={{ padding: 0 }}  >
+					{rowData => {      
+						function handleChange(value) {
+							let updatedLab = { 
+								_id: rowData._id, 
+								//research_id: rowData.research[0],
+								research_id: null,
+								funding: value 
+							};
+							sendFundingUpdate(updatedLab);
+						}          
+						return (
+							<InputNumber 
+								prefix="Funding" 
+								defaultValue={0}
+								disabled={rowData.disableFunding}
+								max={4} 
+								min={0} 
+								step={1} 
+								style={{ width: 140 } }
+								onChange={ handleChange }
+							/>
+						)}}
+					</Cell>
                 </Column>
 
 				<Column verticalAlign='middle' width={230}>
                     <HeaderCell>Cost</HeaderCell>
 					<Cell dataKey="blah">
-					<Input prefix="Cost:"  max={4} min={0} step={1} style={{ width: 140 }}/>
+					{rowData => {      
+						let labUpdates = this.state.labUpdates;
+						const result = newLabCheck(rowData._id, labUpdates);
+						let myFundLevel = 0;
+						let myFunding = 0;
+						if (result >= 0) {	// existing entry
+							myFundLevel = labUpdates[result].funding;
+							myFunding = props.fundingCost[myFundLevel];
+						} 
+						return (
+							<Tag 
+								color="green">
+								$ { myFunding } MB
+							</Tag>
+						)}}
 					</Cell>
                 </Column>
                 </Table>
