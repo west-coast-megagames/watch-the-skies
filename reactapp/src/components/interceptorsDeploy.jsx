0b0f864a24e753a5452c755e1fb0a8e3f2d967d1
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-02-23 06:38:00 -0800

    UPDATED - Deployment drawer

--- reactapp/src/components/interceptorsDeploy.jsx
+++ reactapp/src/components/interceptorsDeploy.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Drawer, Button, InputPicker } from 'rsuite'
+import { Drawer, Button, InputPicker, FlexboxGrid } from 'rsuite'
 import { gameServer } from '../config';
 import axios from 'axios';
 
@@ -15,20 +15,43 @@ let missions = [
 class InterceptorDeployForm extends Component {
 
   state = {
-    ships: [],
+    aircraft: [],
     interceptor: undefined,
     target: this.props.target,
+    mission: undefined,
+    groundMission: false,
     siteMissions: [],
     airMissions: []
   }
 
   componentDidMount() {
-      let siteMissions = missions.filter(mission => mission.site === true)
-      let airMissions = missions.filter(mission => mission.aircraft === true)
-      this.setState({ siteMissions, airMissions });
+      let siteMissions = missions.filter(mission => mission.site === true);
+      let airMissions = missions.filter(mission => mission.aircraft === true);
+      let ships = this.props.aircrafts.filter(aircraft => aircraft.status.deployed !== true);
+      let aircraft = [];
+      for (let ship of ships) {
+        let data = { 
+          label: `${ship.name} (${ ship.country.name } | ${100 - Math.round(ship.stats.hull / ship.stats.hullMax * 100)}% damage)`,
+          value: ship._id,
+        }
+        aircraft.push(data);
+      }
+      this.setState({ siteMissions, airMissions, aircraft });
   }
 
   render() {
+    let { country, zone, model, name, type } = this.props.target;
+    let lat = ''
+    let long = ''
+    if (model === 'Site') {
+      let { latDMS, longDMS } = this.props.target.geoDMS
+      lat = latDMS.split(" ");
+      lat = `${lat[0]}° ${lat[1]}' ${lat[2]}" ${lat[3]}`
+      long = longDMS.split(" ");
+      long = `${long[0]}° ${long[1]}' ${long[2]}" ${long[3]}`
+    };
+    
+    
     return(
       <Drawer
         size='md'
@@ -36,20 +59,55 @@ class InterceptorDeployForm extends Component {
         onHide={() => this.props.deployInterceptors( 'cancel' )}
       >
         <Drawer.Header>
-          <Drawer.Title>Target Information</Drawer.Title>
+          <Drawer.Title>{type} - {name}</Drawer.Title>
         </Drawer.Header>
         <Drawer.Body>
-            <form>
-              <div className="form-group">
-                  <select className="form-control" form="deployForm" value={ this.state.interceptor } onChange={ this.handleChange }>
-                    <option>Scramble aircraft to fly mission over { this.props.target.country.name }</option>
-                    { this.props.aircrafts.filter(aircraft => aircraft.status.deployed !== true).map(ship => (
-                        <option key={ship._id} value={ship._id}>{ ship.name } ( { ship.country.name } | { 100 - Math.round(ship.stats.hull / ship.stats.hullMax * 100) }% damage)</option>
-                    ))}
-                  </select>
-                  <InputPicker placeholder="Mission Selection" data={this.state.target.model === 'Site' ? this.state.siteMissions : this.state.airMissions} value={this.state.mission} onChange={value => (this.setState({ mission: value }))}block />
+        <FlexboxGrid>
+          <FlexboxGrid.Item colspan={24}>
+            <h6>Mission Target - Information</h6>
+            <hr />
+          </ FlexboxGrid.Item>
+          <FlexboxGrid.Item colspan={12}>
+            <p><b>Location:</b> {model === "Aircraft" ? `${country.name} Airspace` : `${country.name}`} - {zone.zoneName}</p>
+          </FlexboxGrid.Item>
+          <FlexboxGrid.Item colspan={12}>
+            {model === "Aircraft" && <p><b>Projected Destination:</b> Unknown...</p>}
+            {model === "Site" && <p>{lat}, {long}</p>}
+          </FlexboxGrid.Item>
+          <FlexboxGrid.Item colspan={24}>
+          <br /><br /><br />
+          <h6>Air Mission - Mission Perameters</h6>
+          <hr />
+          </FlexboxGrid.Item>
+          <FlexboxGrid.Item colspan={12}>
+            <InputPicker placeholder={`Scramble a aircraft over ${country.name}`} data={this.state.aircraft} labelKey='label' value={this.state.interceptor} valueKey='value' onChange={value => (this.setState({ interceptor: value }))} block />
+          </FlexboxGrid.Item>
+          <FlexboxGrid.Item colspan={12}>
+            <InputPicker 
+              placeholder="Mission Type Selection"
+              disabled={this.state.interceptor === undefined} 
+              data={this.state.target.model === 'Site' ? this.state.siteMissions : this.state.airMissions}
+              value={this.state.mission}
+              onChange={value => (this.setState({ mission: value }))}
+              block
+            />
+          </FlexboxGrid.Item>
+          </FlexboxGrid>
+          <br /><br /><br />
+          { model === 'Site' && this.state.interceptor !== undefined && this.state.groundMission &&
+            <React.Fragment>
+            <b>Ground Mission - Mission Perameters</b>
+            <hr />
+            <div className='row' style={{ display: 'flex', flex: '80%' }}>
+              <div className='col' style={{ flex: '50%' }}>
+                <InputPicker placeholder={`Ground Mission`} data={this.state.aircraft} labelKey='label' value={this.state.interceptor} valueKey='value' onChange={value => (this.setState({ interceptor: value }))} block />
               </div>
-            </form>
+              <div className='col' style={{ flex: '50%' }}>
+                <InputPicker placeholder="Mission Type Selection" data={this.state.target.model === 'Site' ? this.state.siteMissions : this.state.airMissions} value={this.state.mission} onChange={value => (this.setState({ mission: value }))} block />
+              </div>
+            </div>
+          </React.Fragment>
+          }
         </Drawer.Body>
         <Drawer.Footer>
           <Button onClick={ this.handleSubmit } appearance="primary">Confirm</Button>
@@ -82,6 +140,7 @@ class InterceptorDeployForm extends Component {
     console.log(`Handling change...`)
     let interceptor = this.state.interceptor;
     interceptor = event.currentTarget.value;
+
     this.setState({
       interceptor
     });
