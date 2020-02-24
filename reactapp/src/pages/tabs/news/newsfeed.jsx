1904efc523fdfb5eba48454d164bb924e802fb02
pdunkerly <pdunkerly@sbcglobal.net>
2020-02-23 22:52:57 -0800

    commit

--- reactapp/src/pages/tabs/news/newsfeed.jsx
+++ reactapp/src/pages/tabs/news/newsfeed.jsx
@@ -1,29 +1,41 @@
-import React from 'react'; // React import
-import { Container } from 'rsuite';
-import { Panel, PanelGroup } from 'rsuite';
+import React from "react"; // React import
+import { Container } from "rsuite";
+import { Panel, PanelGroup } from "rsuite";
 
-const NewsFeed = (props) => {
-    if (props.articles.length === 0) {
-        return(
-            <h5>No articles published by { props.agency }</h5>
-        )
-    };
-    
-    return (
-        <Container >
-            <h5>{ props.agency } News Feed</h5>
-            <PanelGroup style={{ paddingTop: 10 }}>
-                { props.articles.map( article => (
-                    <div  className="artCont" key={ article._id }>
-                        <button onClick={ () => props.del(article) } className="btn btn-danger btn-sm m-1">X</button>
-                        <Panel key={ article._id } header={ article.headline } collapsible bordered>
-                            <p>{ article.body }</p>
-                        </Panel>
-                    </div>
-                ))}
-            </PanelGroup>
-        </Container>
-    );
-}
+const NewsFeed = props => {
+  if (props.articles.length === 0) {
+    return <h5>No articles published by {props.agency}</h5>;
+  }
+
+  //  alternate background colors of headlines for easy differentiation {article.agency} for ID//
+
+  return (
+    <Container>
+      <h5>{props.agency} News Feed</h5>
+      <PanelGroup style={{ paddingTop: 10 }}>
+        {props.articles.map(article => (
+          <div className="artCont" key={article._id}>
+            {/* Individual Agency Logo/Flag here for easy ID by user */}
+            <img src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-round-250.png" />
+            <button
+              onClick={() => props.del(article)}
+              className="btn btn-danger btn-sm m-1"
+            >
+              X
+            </button>
+            <Panel
+              key={article._id}
+              header={article.headline}
+              collapsible
+              bordered
+            >
+              <p>{article.body}</p>
+            </Panel>
+          </div>
+        ))}
+      </PanelGroup>
+    </Container>
+  );
+};
 
 export default NewsFeed;
