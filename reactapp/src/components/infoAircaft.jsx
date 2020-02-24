b47f34de930e0528a2ef3d56fbe6e9281f2e4324
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-02-24 03:05:34 -0800

    MINOR - Change to info window

--- reactapp/src/components/infoAircaft.jsx
+++ reactapp/src/components/infoAircaft.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Drawer, Button, FlexboxGrid, Icon, IconButton, Badge } from 'rsuite'
+import { Drawer, Button, FlexboxGrid, Icon, IconButton, Badge, Tag, TagGroup } from 'rsuite'
 import InterceptorLogs from './logTable';
 
 class InfoAircraft extends Component {
@@ -9,7 +9,7 @@ class InfoAircraft extends Component {
   }
 
   render() {
-    let { stats, name, zone, country, type, baseOrig } = this.props.aircraft;
+    let { stats, status, name, zone, country, type, baseOrig } = this.props.aircraft;
     
     return (
         <Drawer
@@ -24,24 +24,45 @@ class InfoAircraft extends Component {
             <FlexboxGrid>
               <FlexboxGrid.Item colspan={12}>
                 <p><b>Name:</b> { name }</p>
-                <p><b>Location:</b> { country.name } | { zone.zoneName } zone</p>
+                <p><b>Location:</b> { country.name } | { zone.zoneName } zone</p> 
               </FlexboxGrid.Item>
-              <FlexboxGrid.Item>
-                <p><b>Type:</b> { type }</p>
-                <p><IconButton size="xs" icon={<Icon icon="send" />}/> <b>Base:</b> { baseOrig.name }</p>
+              <FlexboxGrid.Item colspan={12}>
+                <p><b>Class:</b> { type }</p>
+                <p><b>Base:</b> { baseOrig.name } <IconButton size="xs" icon={<Icon icon="send" />}>Transfer Aircraft</IconButton></p>
               </FlexboxGrid.Item>
             </FlexboxGrid>
+            <br />
+            <br />
             <FlexboxGrid>
-              <FlexboxGrid.Item>
-                <p><IconButton size="xs" disabled={stats.hull === stats.hullMax} icon={<Icon icon="wrench" />} /> <b>Hull Integrity:</b> { stats.hull }/{ stats.hullMax } {stats.hull < stats.hullMax && <Badge content="Damaged" />}</p> 
-                <p><b>Weapons Rating:</b> { stats.damage }</p>
-                <p><b>Evade Rating:</b> n/a</p>
+              <FlexboxGrid.Item colspan={24}>
+                <h6>Aircraft Statistics</h6>
+                <hr />
               </FlexboxGrid.Item>
-              <FlexboxGrid.Item>
-
+              <FlexboxGrid.Item colspan={12}>
+                <p><IconButton size="xs" icon={<Icon icon="info-circle" />} /> <b>Hull Integrity:</b> { stats.hull }/{ stats.hullMax } {stats.hull < stats.hullMax && <span> <Badge content="Damaged" /> <IconButton size="xs" disabled={stats.hull === stats.hullMax} icon={<Icon icon="wrench" />}>Repair</IconButton></span>}</p> 
+                <p><IconButton size="xs" icon={<Icon icon="info-circle" />} /> <b> Weapons Rating:</b> { stats.attack }</p>
+                <p><IconButton size="xs" icon={<Icon icon="info-circle" />} /> <b> Evade Rating:</b> { stats.evade }</p>
+              </FlexboxGrid.Item>
+              <FlexboxGrid.Item colspan={12}>
+                <p><IconButton size="xs" icon={<Icon icon="info-circle" />} /><b> Armor Rating:</b> { stats.armor }</p>
+                <p><IconButton size="xs" icon={<Icon icon="info-circle" />} /><b> Weapon Pentration:</b> { stats.penetration }</p>
+                <p><IconButton size="xs" icon={<Icon icon="info-circle" />} /> <b> Mission Range:</b> { stats.range }km</p>
+              </FlexboxGrid.Item>
+              <FlexboxGrid.Item colspan={24}>
+                <br />
+                <TagGroup>
+                  {status.ready && <Tag color="green">Mission Ready</Tag>}
+                  {status.deployed && <Tag color="yellow">Deployed</Tag>}
+                  {status.repair && <Tag color="yellow">Repairing</Tag>}
+                  {status.upgrade && <Tag color="yellow">Upgrading</Tag>}
+                  {status.destroyed && <Tag color="red">Destroyed</Tag>}
+                </TagGroup>
               </FlexboxGrid.Item>
             </FlexboxGrid>
-            <h4>Systems</h4>
+            <br />
+            <h6>Systems</h6>
+            <hr />
+
             <ul>
               {this.props.aircraft.systems.map(system => (
                 <li key={system._id}>{system.name} | {system.category}</li>
