1e81ce8c86ffa13cb4159da5756bf489642db7b7
JessicaKriz <51064796+JessicaKriz@users.noreply.github.com>
2020-02-24 02:07:39 -0800

    Wave 1 of Science DataEntry

--- server/models/sci/techResearch.js
+++ server/models/sci/techResearch.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const TechResearch = Research.discriminator('TechResearch', new Schema({
     type: { type: String, default: 'Technology' },
-    field: { type: String, enum: ['Military', 'Consumer', 'Infrastructure', 'Medical', 'Agriculture']},
+    field: { type: String, enum: ['Military', 'Infrastructure', 'Biomedical', 'Agriculture']},
     team: { type: Schema.Types.ObjectId, ref: 'Team'},
     status: {
         progress: { type: Number, default: 0 },
