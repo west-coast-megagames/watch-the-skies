334135d183a102083db15517129c8f5a273caed8
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-02-24 20:05:18 -0800

    FIXED - Data break in science

--- server/models/sci/research.js
+++ server/models/sci/research.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const UnlockSchema = new Schema({
+    code: { type: String },
+    type: { type: String }
+  });
+
 const ResearchSchema = new Schema({
     model: { type: String, default: 'Research'},
     name: { type: String },
@@ -8,7 +13,7 @@ const ResearchSchema = new Schema({
     level: { type: Number },
     prereq: [String],
     desc: { type: String },
-    unlocks: [String]
+    unlocks: [UnlockSchema]
 });
 
 module.exports = Log = mongoose.model('Research', ResearchSchema, 'research');
