97f8fe53bacbea6a9561fe223fb276d2cde7e10c
pdunkerly <pdunkerly@sbcglobal.net>
2020-02-24 22:44:14 -0800

    commit

--- reactapp/src/pages/tabs/news/newsfeed.jsx
+++ reactapp/src/pages/tabs/news/newsfeed.jsx
@@ -1,6 +1,7 @@
 import React from "react"; // React import
 import { Container } from "rsuite";
 import { Panel, PanelGroup } from "rsuite";
+import { Avatar } from "rsuite";
 
 const NewsFeed = props => {
   if (props.articles.length === 0) {
@@ -15,14 +16,28 @@ const NewsFeed = props => {
       <PanelGroup style={{ paddingTop: 10 }}>
         {props.articles.map(article => (
           <div className="artCont" key={article._id}>
-            {/* Individual Agency Logo/Flag here for easy ID by user */}
-            <img src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-round-250.png" />
             <button
               onClick={() => props.del(article)}
               className="btn btn-danger btn-sm m-1"
+              style={{ float: "right", display: "inline-block" }}
             >
-              X
+              Trash
             </button>
+
+            {/* Individual Agency Logo/Flag here for easy ID by user */}
+            {article.agency === "GNN" && (
+              <img
+                src="../../../img/GNN_logo.svg"
+                style={{ maxWidth: "30px" }}
+              />
+            )}
+            {article.agency === "US" && (
+              <img
+                src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-round-250.png"
+                style={{ maxWidth: "30px" }}
+              />
+            )}
+
             <Panel
               key={article._id}
               header={article.headline}
