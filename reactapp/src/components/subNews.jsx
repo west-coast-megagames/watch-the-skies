8d8e8e6bd2606281fb33ccf10c1f4cf484538156
pdunkerly <pdunkerly@sbcglobal.net>
2020-02-26 23:03:03 -0800

    article prettier

--- reactapp/src/components/subNews.jsx
+++ reactapp/src/components/subNews.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import axios from 'axios'
-import { gameServer } from '../config'
-import { Form, FormGroup, FormControl, Button, ButtonToolbar, SelectPicker, Input } from 'rsuite';
+import React from "react";
+import axios from "axios";
+import { gameServer } from "../config";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  Button,
+  ButtonToolbar,
+  SelectPicker,
+  Input
+} from "rsuite";
 //import Select from '../components/common/selectPicker';
 
 /*class TextField extends React.PureComponent {
@@ -15,8 +23,6 @@ import { Form, FormGroup, FormControl, Button, ButtonToolbar, SelectPicker, Inpu
     );
   }
 }*/
-
-
 
 class SubNews extends React.Component {
   /*constructor(props) {
@@ -35,27 +41,27 @@ class SubNews extends React.Component {
   }
   */
 
-    state = {
+  state = {
     article: {
       agency: "",
       location: "",
       headline: "",
       body: "",
       imageSrc: ""
-    }}
+    }
+  };
 
-    handleInput = (value, id) => {
-        console.log(this);
-        console.log(value);
-        console.log(id);
+  handleInput = (value, id) => {
+    console.log(this);
+    console.log(value);
+    console.log(id);
 
-        let article = this.state.article;
-        article[id] = value;
-        this.setState({ article });
-    };
+    let article = this.state.article;
+    article[id] = value;
+    this.setState({ article });
+  };
 
-
-/*
+  /*
   async handleSubmit() {
     console.log('Form submitted...');
     console.dir(this.state.formValue);
@@ -79,61 +85,111 @@ class SubNews extends React.Component {
   }
   */
 
-    handleSubmit = async e => {
-        e.preventDefault();
-        try {
-            let resArticle = await axios.post(`${gameServer}api/news`, this.state.article);
-            //localStorage.setItem('token', res.headers['x-auth-token']);
-            this.props.alert({
-                type: 'success',
-                title: 'News Item Submitted',
-                body: `News with headline:  ${resArticle.data.headline}!`
-            }) 
-        } catch (err) {
-            console.log(err)
-            this.props.alert({
-                type: 'error',
-                title: 'Failed to create news item',
-                body: `${err.message}`})
-        }
+  handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      let resArticle = await axios.post(
+        `${gameServer}api/news`,
+        this.state.article
+      );
+      //localStorage.setItem('token', res.headers['x-auth-token']);
+      this.props.alert({
+        type: "success",
+        title: "News Item Submitted",
+        body: `News with headline:  ${resArticle.data.headline}!`
+      });
+    } catch (err) {
+      console.log(err);
+      this.props.alert({
+        type: "error",
+        title: "Failed to create news item",
+        body: `${err.message}`
+      });
     }
+  };
 
   handleCheckNews() {
-    this.form.checkForField('news', checkResult => {
+    this.form.checkForField("news", checkResult => {
       console.log(checkResult);
     });
   }
   render() {
-    const { body, headline, imageSrc  } = this.state.article;
-
+    const { body, headline, imageSrc } = this.state.article;
 
     const agency = [
-        {value: 'BNC', label: 'BNC'},
-        {value: 'GNN', label: 'GNN'},
-        {value: 'US', label: 'US'}
+      { value: "BNC", label: "BNC" },
+      { value: "GNN", label: "GNN" },
+      { value: "USA", label: "USA" }
     ];
 
     return (
       <Form>
         <FormGroup>
-          <SelectPicker id="agency" data={agency} style={{ width: 224 }}  labelKey='label' valueKey='value' placeholder='pick your origin' onChange={(value)=>this.handleInput(value, 'agency')}/>
-          <SelectPicker id="agency" data={agency} style={{ width: 224 }}  labelKey='label' valueKey='value' placeholder='pick your origin' onChange={(value)=>this.handleInput(value, 'agency')}/>
+          <SelectPicker
+            id="agency"
+            data={agency}
+            style={{ width: 224 }}
+            labelKey="label"
+            valueKey="value"
+            placeholder="pick your origin"
+            onChange={value => this.handleInput(value, "agency")}
+          />
+          <SelectPicker
+            id="agency"
+            data={agency}
+            style={{ width: 224 }}
+            labelKey="label"
+            valueKey="value"
+            placeholder="pick your origin"
+            onChange={value => this.handleInput(value, "agency")}
+          />
         </FormGroup>
 
         <FormGroup>
-          <Input id='location' type="text" value={this.state.article.location} name="location" label="Location"  placeholder='location'  onChange={(value)=>this.handleInput(value, 'location')}/>
+          <Input
+            id="location"
+            type="text"
+            value={this.state.article.location}
+            name="location"
+            label="Location"
+            placeholder="location"
+            onChange={value => this.handleInput(value, "location")}
+          />
         </FormGroup>
 
         <FormGroup>
-          <Input id='headline' type="text" value={headline} name="headline" label="Headline"  placeholder='headline' onChange={(value)=>this.handleInput(value, 'headline')} />
+          <Input
+            id="headline"
+            type="text"
+            value={headline}
+            name="headline"
+            label="Headline"
+            placeholder="headline"
+            onChange={value => this.handleInput(value, "headline")}
+          />
         </FormGroup>
 
         <FormGroup>
-          <FormControl id='body' componentClass="textarea" rows={6} name="body" value={body} onChange={(value)=>this.handleInput(value, 'body')} />
+          <FormControl
+            id="body"
+            componentClass="textarea"
+            rows={6}
+            name="body"
+            value={body}
+            onChange={value => this.handleInput(value, "body")}
+          />
         </FormGroup>
-        
+
         <FormGroup>
-        <Input id='imageSrc' type="text" value={imageSrc} name="img" label="Image Source" placeholder='img' onChange={(value)=>this.handleInput(value, 'imageSrc')} />
+          <Input
+            id="imageSrc"
+            type="text"
+            value={imageSrc}
+            name="img"
+            label="Image Source"
+            placeholder="img"
+            onChange={value => this.handleInput(value, "imageSrc")}
+          />
         </FormGroup>
 
         <ButtonToolbar>
--- reactapp/src/pages/tabs/news/newsfeed.jsx
+++ reactapp/src/pages/tabs/news/newsfeed.jsx
@@ -1,6 +1,7 @@
 import React from "react"; // React import
 import { Container } from "rsuite";
 import { Panel, PanelGroup } from "rsuite";
+import TeamAvatar from "../../../components/common/teamAvatar";
 
 const NewsFeed = props => {
   if (props.articles.length === 0) {
@@ -14,7 +15,17 @@ const NewsFeed = props => {
       <h5>{props.agency} News Feed</h5>
       <PanelGroup style={{ paddingTop: 10 }}>
         {props.articles.map(article => (
-          <div className="artCont" key={article._id}>
+          <div
+            className="artCont"
+            key={article._id}
+            style={{
+              border: "solid 2px #d9d9d9",
+              margin: 10,
+              padding: "5px 2px 0px 5px",
+              borderRadius: "6px 6px 6px 6px",
+              backgroundColor: "#f2f2f2"
+            }}
+          >
             <button
               onClick={() => props.del(article)}
               className="btn btn-danger btn-sm m-1"
@@ -24,11 +35,14 @@ const NewsFeed = props => {
             </button>
 
             {/* Individual Agency Logo/Flag here for easy ID by user */}
+            <TeamAvatar size={"sm"} teamCode={article.agency} />
+
+            {/* OLD WAY
             {article.agency === "GNN" && (
-              <img
-                src="../../../img/GNN_logo.svg"
-                style={{ maxWidth: "30px" }}
-              />
+              <img src={GNN} style={{ maxWidth: "30px" }} />
+            )}
+            {article.agency === "BNC" && (
+              <img src={BNC} style={{ maxWidth: "30px" }} />
             )}
             {article.agency === "US" && (
               <img
@@ -36,6 +50,13 @@ const NewsFeed = props => {
                 style={{ maxWidth: "30px" }}
               />
             )}
+
+            USA: "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-round-250.png",
+                RFD: "https://cdn.countryflags.com/thumbs/russia/flag-square-250.png",
+                PRC: "https://cdn.countryflags.com/thumbs/china/flag-square-250.png",
+                TUK: "https://cdn.countryflags.com/thumbs/united-kingdom/flag-square-250.png",
+                TFR: "https://cdn.countryflags.com/thumbs/france/flag-square-250.png",
+                FFG: "" */}
 
             <Panel
               key={article._id}
