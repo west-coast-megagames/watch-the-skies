bf5bdfbddd8c6ffadc5f4ecf40a98ee5b71b421a
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-02-29 04:18:12 -0800

    ADDED - Report Classes

--- server/wts/reports/reportClasses.js
+++ server/wts/reports/reportClasses.js
@@ -2,12 +2,15 @@ const reportDebugger = require('debug')('app:reports');
 const ResearchLog = require('../../models/logs/researchLog');
 const ReconLog = require('../../models/logs/reconLog');
 
+// Function that makes a timestamp for log files
 function makeTimestamp() {
     const gameClock = require('../gameClock/gameClock')
     let { turn, phase, turnNum, minutes, seconds } = gameClock.getTimeRemaining();
     let timestamp = { timestamp: { turn, phase, turnNum, clock: `${minutes}:${seconds}` }}
     return timestamp;
 }
+
+// After Action Report for Lab activity
 class ResearchReport {
     constructor() {
         this.date = ''
@@ -35,6 +38,7 @@ class ResearchReport {
         try {
         reportDebugger(`Saving report!`);
             let timestamp = makeTimestamp();
+            this.date = Date.now();
             let submission = new ResearchLog({...timestamp,...this})
 
             submission = await submission.save();
@@ -48,6 +52,7 @@ class ResearchReport {
     }
 };
 
+// After Action Report for Recon Missions (Ground/Air)
 class ReconReport {
     constructor() {
         this.date = ''
@@ -65,6 +70,7 @@ class ReconReport {
         try {
             reportDebugger(`Saving Recon report!`);
             let timestamp = makeTimestamp();
+            this.date = Date.now();
             let submission = new ReconLog({...timestamp,...this});
 
             submission = await submission.save();
@@ -78,4 +84,75 @@ class ReconReport {
     }
 };
 
-module.exports = { ResearchReport, ReconReport };
+class TransportReport {
+    constructor() {
+        this.date = ''
+        this.team = {}
+        this.unit = {}
+        this.report = {}
+        this.site = {}
+        this.country = {}
+        this.zone = {}
+
+        this.saveReport = this.saveReport.bind(this);
+    }
+
+    async saveReport() {
+        try {
+            reportDebugger(`Saving Transport report!`);
+            let timestamp = makeTimestamp();
+            this.date = Date.now();
+            let submission = new TransportLog({...timestamp,...this});
+
+            submission = await submission.save();
+            reportDebugger(submission);
+
+            return;
+        } catch (err) {
+            reportDebugger(`Transport Log Error: ${err}`);
+            return
+        }
+    }
+};
+
+class BattleReport {
+    constructor() {
+        this.team = {}
+        this.attackers = []
+        this.defenders = []
+        this.stats = {}
+        this.stats.atkTotal = 0
+        this.stats.atkDamage = 0
+        this.stats.defTotal = 0
+        this.stats.defDamage = 0
+        this.casualties = []
+        this.salvage = []
+        this.report = {}
+        this.site = {}
+        this.country = {}
+        this.zone = {}
+
+        this.saveReport = this.saveReport.bind(this);
+    }
+
+    async saveReport() {
+        try {
+            reportDebugger(`Saving Battle report!`);
+            let timestamp = makeTimestamp();
+            this.date = Date.now();
+            let submission = new TransportLog({...timestamp,...this});
+
+            submission = await submission.save();
+            reportDebugger(submission);
+
+            return;
+        } catch (err) {
+            reportDebugger(`Transport Log Error: ${err}`);
+            return
+        }
+    }
+};
+
+
+
+module.exports = { ResearchReport, ReconReport, TransportReport, BattleReport };
