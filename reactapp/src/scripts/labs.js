14399a2afefd329bb10dea514a0c7da7b274dff9
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-02-28 20:29:18 -0800

    ADDED - script

---
+++ reactapp/src/scripts/labs.js
@@ -0,0 +1,11 @@
+function newLabCheck(lab, labArray) {
+	let i;
+	for (i = 0; i < labArray.length; i++) {
+		if (labArray[i]._id === lab) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+export default newLabCheck;
