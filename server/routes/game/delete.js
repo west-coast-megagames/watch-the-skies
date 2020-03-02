ad0068eb33e573487ad29064ae4a474d9f52497e
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-03-02 03:00:43 -0800

    ADDED - Delete route

---
+++ server/routes/game/delete.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const nexusEvent = require('../../startup/events');
+const routeDebugger = require('debug')('app:routes:admin');
+
+const Log = require('../../models/logs/log');
+
+// @route   DELETE game/delete/logs
+// @desc    DELETE all LOG files
+// @access  Public
+router.delete('/logs', async function (req, res) {
+    let data = await Log.deleteMany();
+    console.log(data);
+    res.status(200).send(`We wiped out ${data.deletedCount} records in the Logs Database!`);
+});
+
+module.exports = router
+
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 // Routes - Using Express
 const game = require('../routes/game/game');
 const admin = require('../routes/game/admin');
+const del = require('../routes/game/delete');
 const home = require('../routes/api/home');
 const auth = require('../routes/api/auth');
 const interceptor = require('../routes/api/interceptor');
@@ -39,6 +40,7 @@ module.exports = function(app) {
     app.use('/', home);
     app.use('/game', game);
     app.use('/game/admin', admin);
+    app.use('/game/delete', del);
     app.use('/api/auth', auth);
     app.use('/api/interceptor', interceptor); // Route for manipulating interceptors
     app.use('/api/team', team); // Route for Teams
