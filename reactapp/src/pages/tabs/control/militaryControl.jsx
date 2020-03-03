e206b4411966e66a8ea3016573f7b9606dfd86bd
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-03-03 04:51:07 -0800

    ADDED - Deployment Modal

--- reactapp/src/pages/tabs/control/militaryControl.jsx
+++ reactapp/src/pages/tabs/control/militaryControl.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Alert, Container, Header, Content, Sidebar, ButtonToolbar, Button, Icon, IconButton, Drawer, SelectPicker, CheckPicker, Divider, Toggle } from 'rsuite';
+import { Alert, Container, Header, Content, Sidebar, ButtonToolbar, Button, Icon, IconButton, Drawer, SelectPicker, CheckPicker, Divider, Toggle, Tag } from 'rsuite';
+import { gameServer } from '../../../config';
+import axios from 'axios';
 
 class MilitaryControl extends Component {
     state = {
@@ -7,20 +9,31 @@ class MilitaryControl extends Component {
         showInvade: false,
         team: null,
         units: [],
-        desination: [],
+        destination: [],
         corpsUnits: [],
         fleetUnits: [],
+        citySites: [],
+        baseSites: [],
         seaDeploy: false,
-        sort: false
+        cost: 0
     }
 
     componentWillMount() {
         this.filterUnits();
+        this.filterLocations();
     }
 
-    componentDidUpdate(prevState) {
-        if (this.prevState.team !== this.state.team) {
-            this.filterUnits();
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.units.length !== prevState.units.length) {
+            let cost = 0 // Gets the current displayed cost
+            let targetSite = this.props.sites.find(el => el._id === this.state.destination); // Looks up the target site via the stored _id
+            Alert.warning(targetSite.name) // Gives me site name
+            for (let unit of this.state.units) {
+                unit = this.props.military.find(el => el._id === unit) // Looks up current unit
+                if (unit.zone.zoneName === targetSite.zone.zoneName) { cost += unit.stats.localDeploy };
+                if (unit.zone.zoneName !== targetSite.zone.zoneName) { cost += unit.stats.globalDeploy }; 
+            }
+            this.setState({cost})
         }
     }
 
@@ -28,29 +41,39 @@ class MilitaryControl extends Component {
     closeDeploy = () => { this.setState({showDeploy: false}) };
     showInvade = () => { this.setState({showInvade: true}) };
     closeInvade = () => { this.setState({showInvade: false}) };
-    handleTeam = (value) => {this.setState({team: value, sort: true}); this.filterUnits();};
-    handleType = (value) => { this.setState({seaDeploy: value})};
+    handleTeam = (value) => { this.setState({team: value}); this.filterUnits();};
+    handleType = (value) => { this.setState({seaDeploy: value, units: [], destination: []})};
+    handleDestination = (value) => { this.setState ({destination: value})};
     handleUnits = (value) => { this.setState({units: value})}
 
     filterUnits = () => {
-        Alert.warning(`Filtering!`)
+        Alert.warning(`Filtering Units!`);
         let data = []
         let military = this.props.military;
         for (let unit of military) {
             unit.checkZone = unit.zone.zoneName;
-            unit.info = `${unit.name} - Health: ${unit.stats.health}/${unit.stats.healthMax} | Atk: ${unit.stats.attack} | Def: ${unit.stats.defense}`
+            unit.info = `${unit.name} - Hlth: ${unit.stats.health}/${unit.stats.healthMax} | Atk: ${unit.stats.attack} | Def: ${unit.stats.defense}`
             data.push(unit);
         }
-
-        if (this.state.sort) {
-            data.filter( el => el.team.name === this.state.team.name)
-            let sort = false
-            this.setState({sort})
-        } 
             
         let corpsUnits = data.filter( el => el.type === 'Corps')
         let fleetUnits = data.filter( el => el.type === 'Fleet')
         this.setState({corpsUnits, fleetUnits});
+    }
+
+    filterLocations = () => {
+        Alert.warning('Filtering Locations')
+        let data = []
+        let sites = this.props.sites;
+        for (let site of sites) {
+            site.checkZone = site.zone.zoneName;
+            site.info = `${site.country.name} - ${site.name} | ${site.team.shortName}`
+            data.push(site);
+        }
+            
+        let citySites = data.filter( el => el.type === 'Base')
+        let baseSites = data.filter( el => el.type === 'City')
+        this.setState({citySites, baseSites});
     }
 
     render() {
@@ -76,9 +99,10 @@ class MilitaryControl extends Component {
                 </Sidebar>
                 <Drawer size='sm'  placement='right' show={showDeploy} onHide={this.closeDeploy}>
                     <Drawer.Header>
-                        <Drawer.Title>Military Deployment</Drawer.Title>
+                        <Drawer.Title>Military Deployment<Tag style={{ float: 'right' }} color="green">{this.props.accounts.length > 0 ? `Deployment Cost: $M${this.state.cost}` : `Deployment Cost: 0`}</Tag></Drawer.Title>
                     </Drawer.Header>
                     <Drawer.Body>
+                        <h6>Select Team</h6>
                         <SelectPicker block placeholder='Select Team'
                             data={this.props.teams.filter(el => el.teamType === 'N')}
                             labelKey='name'
@@ -87,16 +111,25 @@ class MilitaryControl extends Component {
                             value={this.state.team}
                         />
                         <Divider />
-                        {this.state.seaDeploy && <CheckPicker block placeholder='Select Units'
-                            data={this.state.fleetUnits}
+                        <h6>Select Destination</h6>
+                        <SelectPicker block disabled={this.state.team == null} placeholder='Select Destination'
+                            data={[...this.state.citySites,...this.state.baseSites]}
+                            onChange={this.handleDestination}
+                            valueKey='_id'
+                            labelKey='info'
+                            groupBy='checkZone'
+                        />
+                        <Divider />
+                        <h6>Select Units</h6>
+                        {this.state.seaDeploy && <CheckPicker block disabled={this.state.team == null || this.state.destination == null} placeholder='Select Units'
+                            data={this.state.fleetUnits.filter( el => el.team.name === this.state.team)}
                             onChange={this.handleUnits}
                             valueKey='_id'
                             labelKey='info'
                             groupBy='checkZone'
                         />}
-
-                        {!this.state.seaDeploy && <CheckPicker block placeholder='Select Units'
-                            data={this.state.corpsUnits}
+                        {!this.state.seaDeploy && <CheckPicker block disabled={this.state.team == null || this.state.destination == null} placeholder='Select Units'
+                            data={this.state.corpsUnits.filter( el => el.team.name === this.state.team)}
                             onChange={this.handleUnits}
                             valueKey='_id'
                             labelKey='info'
@@ -105,13 +138,25 @@ class MilitaryControl extends Component {
                     </Drawer.Body>
                     <Drawer.Footer>
                         <Toggle style={{float: 'left'}} onChange={this.handleType} size="lg" checkedChildren="Sea Deploy" unCheckedChildren="Land Deploy" />
-                        <Button onClick={this.closeDeploy} appearance="primary">Confirm</Button>
+                        <Button onClick={this.submitDeployment} appearance="primary">Confirm</Button>
                         <Button onClick={this.closeDeploy} appearance="subtle">Cancel</Button>
                     </Drawer.Footer>
                 </Drawer>
             </Container>
         );
     }
+
+    submitDeployment = async () => {
+        let { cost, units, destination, team } = this.state;
+        let deployment = { cost, units, destination, team };
+
+        try {
+            let { data } = await axios.put(`${gameServer}game/military/deploy`, deployment); // Axios call to deploy units
+            Alert.success(data.body)
+        } catch (err) {
+            Alert.error(`Error: ${err.body} ${err.message}`, 5000)
+        }
+    }   
 }
  
 export default MilitaryControl;
