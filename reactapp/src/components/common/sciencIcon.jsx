132a3f027566742c77b481578d0cbfb31fd19277
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-03-28 15:40:35 -0700

    FIXED - sci icon compile error

--- reactapp/src/components/common/sciencIcon.jsx
+++ reactapp/src/components/common/sciencIcon.jsx
@@ -4,7 +4,6 @@ import flask1 from '../../img/sci1.svg'
 import flask2 from '../../img/sci2.svg'
 import flask3 from '../../img/sci3.svg'
 import flask4 from '../../img/sci4.svg'
-import flask5 from '../../img/sci5.svg'
 
 const SciIcon = (props) => {
     return (
@@ -14,7 +13,7 @@ const SciIcon = (props) => {
             {props.level === 2 && <img style={{height: props.size, width: props.size}} src={flask2} alt='Level 2' />}
             {props.level === 3 && <img style={{height: props.size, width: props.size}} src={flask3} alt='Level 3' />}
             {props.level === 4 && <img style={{height: props.size, width: props.size}} src={flask4} alt='Level 4' />}
-            {props.level === 5 && <img style={{height: props.size, width: props.size}} src={flask5} alt='Level 5' />}
+            {props.level === 5 && <img style={{height: props.size, width: props.size}} src={flask4} alt='Level 5' />}
         </React.Fragment>
     );
 }
