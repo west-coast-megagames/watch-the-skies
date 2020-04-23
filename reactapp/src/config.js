f714c0913591eb8b1c9564873bc64fb15c7656a1
jeffwqc <jeffwqc@gmail.com>
2020-04-23 06:17:30 -0700

    ending quote added

--- reactapp/src/config.js
+++ reactapp/src/config.js
@@ -1,7 +1,7 @@
-let gameServer = 'http://localhost:5000/;
+let gameServer = "http://localhost:5000/";
 
 if (process.env.NODE_ENV === "production") {
-  gameServer = 'https://project-nexus-prototype.herokuapp.com/';
-} 
+  gameServer = "https://project-nexus-prototype.herokuapp.com/";
+}
 
 export { gameServer };
