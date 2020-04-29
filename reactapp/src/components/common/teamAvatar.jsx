fafcaecd428f99b21b7f119baa2de1a9500846ee
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-04-28 19:16:18 -0700

    MINOR - Changes to news

--- reactapp/src/components/common/teamAvatar.jsx
+++ reactapp/src/components/common/teamAvatar.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Avatar } from "rsuite";
+import BNC_logo from "../../img/BNC_logo.png";
+import GNN_logo from "../../img/GNN_logo.png"
 
 // TODO for March - Get images in for each of the 15 teams by code.
 
@@ -10,8 +12,8 @@ const team = {
   TUK: "https://cdn.countryflags.com/thumbs/united-kingdom/flag-square-250.png",
   TFR: "https://cdn.countryflags.com/thumbs/france/flag-square-250.png",
   FFG: "",
-  BNC: "../../img/BNC_logo.png",
-  GNN: "../../img/GNN_logo.png"
+  BNC: BNC_logo,
+  GNN: GNN_logo
 };
 
 class TeamAvatar extends Component {
