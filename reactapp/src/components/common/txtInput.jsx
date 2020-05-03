cf685ee4626fda3c60fbba39f7bf918e5dac2a6e
jeffwqc <jeffwqc@gmail.com>
2020-05-03 14:54:09 -0700

    allow passing of type password later in txtInput

--- reactapp/src/components/common/txtInput.jsx
+++ reactapp/src/components/common/txtInput.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const TxtInput = ({ name, label, value, error, onChange }) => {
-    return (
+const TxtInput = ({ name, label, value, error, onChange, type = "text" }) => {
+  return (
     <div className="form-group">
-        <label htmlFor={ name }>{ label }</label>
-        <input 
-            autoFocus
-            value={ value }
-            onChange={ onChange }
-            id={ name }
-            name={ name }
-            type="text"
-            className="form-control"
-        />
-        {error && <div className="alert alert-danger">{error}</div>}
+      <label htmlFor={name}>{label}</label>
+      <input
+        autoFocus
+        value={value}
+        onChange={onChange}
+        id={name}
+        name={name}
+        type={type}
+        className="form-control"
+      />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
-    )
-;}
- 
+  );
+};
+
 export default TxtInput;
--- reactapp/src/components/loginForm.jsx
+++ reactapp/src/components/loginForm.jsx
@@ -1,109 +1,116 @@
-import React, { Component } from 'react';
-import TxtInput from './common/txtInput';
-import Joi from 'joi-browser';
-import axios from 'axios'
-import { gameServer } from '../config';
+import React, { Component } from "react";
+import TxtInput from "./common/txtInput";
+import Joi from "joi-browser";
+import axios from "axios";
+import { gameServer } from "../config";
 
-class LiginForm extends Component {
-    state = {
-        account: { login: '', password: ''},
-        errors: {}
-    };
+class LoginForm extends Component {
+  state = {
+    account: { login: "", password: "" },
+    errors: {},
+  };
 
-    schema = {
-        login: Joi.string().required(),
-        password: Joi.string().required()
-    };
+  schema = {
+    login: Joi.string().required(),
+    password: Joi.string().required(),
+  };
 
-    validate = () => {
-        const result = Joi.validate(this.state.account, this.schema, { abortEarly: false });
-        console.log(result)
-        
-        const errors = {};
+  validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    console.log(result);
 
-        const { account } = this.state
+    const errors = {};
 
-        if (account.login.trim() === '')
-            errors.login = "Username or email is required."
-        
-        if (account.password.trim() === '')
-            errors.password = "Password is required"
+    const { account } = this.state;
 
-        return Object.keys(errors).length === 0 ? null : errors
-    };
+    if (account.login.trim() === "")
+      errors.login = "Username or email is required.";
 
-    validateProperty = ({ name, value }) => {
-        if (name === 'login') {
-            if (value.trim() === "") return 'Username or email is required!';
-        }
-        if (name === 'password') {
-            if (value.trim() === "") return 'Password is required!';
-        }
-    };
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
 
-    handleSubmit = async e => {
-        e.preventDefault();
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
 
-        // const errors = this.validate();
-        // this.setState({ errors: errors || {} });
-        // if (errors) return;
-
-        // Call the server
-        console.log('Submitted');
-        try {
-            let res = await axios.post(`${gameServer}api/auth`, this.state.account)
-            let jwt = res.data;
-            console.log(`Token: ${jwt}`)
-            this.props.addAlert({
-                type: 'success',
-                title: 'Login Successful',
-                body: `Logged in... welcome ${this.state.account.login}!`})
-            localStorage.setItem('token', jwt);
-            this.props.login();
-            this.props.close();
-        } catch (err) {
-            console.log(`Error: ${err}`);
-            const account = {...this.state.account};
-            const errors = {login : 'Username or password is incorrect'};
-            this.setState({ account, errors }); 
-        }
-        
-    };
-
-    handleChange = ({currentTarget: input}) => {
-        const errors = { ...this.state.errors };
-        const errorMessage = this.validateProperty(input);
-        if (errorMessage) errors[input.name] = errorMessage;
-        else delete errors[input.name];
-
-        const account = {...this.state.account};
-        account[input.name] = input.value;
-        this.setState({ account, errors })
-    };
-
-    render() {
-        const { account, errors } = this.state;
-        return <div>
-            <form onSubmit={this.handleSubmit}>
-                <TxtInput
-                    name="login"
-                    value={account.login}
-                    label="login"
-                    onChange={this.handleChange}
-                    error={errors.login}
-                />
-
-                <TxtInput
-                    name="password"
-                    value={account.password}
-                    label="Password"
-                    onChange={this.handleChange}
-                    error={errors.password}
-                />
-                <button className="btn btn-primary">Login</button>
-            </form>
-        </div>;
+  validateProperty = ({ name, value }) => {
+    if (name === "login") {
+      if (value.trim() === "") return "Username or email is required!";
     }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required!";
+    }
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // const errors = this.validate();
+    // this.setState({ errors: errors || {} });
+    // if (errors) return;
+
+    // Call the server
+    console.log("Submitted");
+    try {
+      let res = await axios.post(`${gameServer}api/auth`, this.state.account);
+      let jwt = res.data;
+      console.log(`Token: ${jwt}`);
+      this.props.addAlert({
+        type: "success",
+        title: "Login Successful",
+        body: `Logged in... welcome ${this.state.account.login}!`,
+      });
+      localStorage.setItem("token", jwt);
+      this.props.login();
+      this.props.close();
+    } catch (err) {
+      console.log(`Error: ${err}`);
+      const account = { ...this.state.account };
+      const errors = { login: "Username or password is incorrect" };
+      this.setState({ account, errors });
+    }
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account, errors });
+  };
+
+  /* eventually change password type from text to password
+  type="password"
+  */
+  render() {
+    const { account, errors } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <TxtInput
+            name="login"
+            value={account.login}
+            label="login"
+            onChange={this.handleChange}
+            error={errors.login}
+          />
+
+          <TxtInput
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+            error={errors.password}
+          />
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
-export default LiginForm;
+
+export default LoginForm;
