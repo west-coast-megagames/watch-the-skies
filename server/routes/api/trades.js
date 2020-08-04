cb068b2ed52f997548920528a6f6b4319d695612
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-08-03 22:13:38 -0700

    FIXED - Trade Route

--- server/routes/api/trades.js
+++ server/routes/api/trades.js
@@ -1,23 +1,17 @@
 const routeDebugger = require('debug')('app:routes');
-
 const express = require('express');
 const router = express.Router();
 
 // Trade Models - Using Mongoose Model
 const { Trade } = require('../../models/dip/trades');
-const validateObjectId = require('../../middleware/validateObjectId');
-
-const { logger } = require('../../middleware/winston'); // Import of winston for error logging
-require ('winston-mongodb');
 
 // @route   GET api/trade/
 // @Desc    Get all trades
 // @access  Public
-
 router.get('/', async function (res, res){
     routeDebugger('Showing all Trades');
-    let trade = await Trade.find().sort({ team: 1 });
-    res.status(200).json(state);   
-    res.json(trade);
+    let trade = await Trade.find().sort({ team: 1 });  
+    res.status(200).json(trade);
 }); 
 
+module.exports = router;
