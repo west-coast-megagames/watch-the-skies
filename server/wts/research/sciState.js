a30ee1c8d855edb67443f1727b0dacd731a4b47a
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-08-17 06:21:08 -0700

    ADDED - multiplier rates to sciState

--- server/wts/research/sciState.js
+++ server/wts/research/sciState.js
@@ -3,8 +3,10 @@ const { loadKnowledge, loadGlobalVariables } = require('./knowledge')
 
 const { logger } = require('../../middleware/winston'); // Import of winston for error logging
 
-const fundingCost = [ 0, 2, 4, 6, 8 ] // A cost of 3 + funding level per roll currently
-const techCost = [ 20, 30, 40, 50, 60, 70 ] // Arbitratily set at increments of 50 currently
+const fundingCost = [ 0, 2, 4, 6, 8 ]; // A cost of 3 + funding level per roll currently
+const techCost = [ 20, 30, 40, 50, 60, 70 ] // Cost for each level of tech, arbitratily set at increments of 10 currently
+
+const multiplier = { setBack: 0.5, normal: 0.25, fast: 0.6, breakthrough: 0.2 }; // Science Bonus Multipliers
 
 initScience()
 
@@ -15,8 +17,6 @@ async function initScience() {
     await loadKnowledge(); // Loads the knowledge tree with current knowledge
     logger.info('Loading Tech into Tech Tree...')
     await loadTech() // Loads the tech-tree with the curren science
-
-
 }
 
-module.exports = { techCost, fundingCost }
+module.exports = { techCost, fundingCost, multiplier }
