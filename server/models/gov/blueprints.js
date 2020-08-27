e89dd32a2ae1951f5ad6bf2ad2086fd09e4a4004
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-08-27 05:12:40 -0700

    ADDED - Blueprint Load addition

--- server/models/gov/blueprints.js
+++ server/models/gov/blueprints.js
@@ -5,29 +5,32 @@ const Joi = require("joi");
 const ObjectId = mongoose.ObjectId;
 
 const BlueprintSchema = new Schema({
-    model: { type: String, required: true, default: "Blueprint" },
-    name: { type: String, required: true, min: 2, maxlength: 50 },
-    code: {
-        type: String,
-        minlength: 2,
-        maxlength: 20,
-        required: true,
-        unique: true,
-      },  
-    cost: { type: Number, required: true, default: 0},
-    buildTime: { type: Number, required: true, default: 0},
-    desc: { type: String, required: true, default: "Blueprint" },
-    prereq: [],
-    hidden: { type: Boolean, required: true, default: false },
-
+  model: { type: String, required: true, default: "Blueprint" },
+  name: { type: String, required: true, min: 2, maxlength: 50 },
+  code: { type: String, minlength: 2, maxlength: 20, required: true, unique: true },  
+  cost: { type: Number, required: true, default: 0 },
+  buildTime: { type: Number, required: true, default: 0 },
+  desc: { type: String, required: true, default: "Blueprint" },
+  prereq: [],
+  hidden: { type: Boolean, required: true, default: false },
 });
 
 let Blueprint = mongoose.model("Blueprint", BlueprintSchema);
 
-const FacilityBlueprint = Facility.discriminator('FacilityBlueprint', new Schema({
-    capability: { type: Schema.Types.Mixed },
+const FacilityBlueprint = Blueprint.discriminator('FacilityBlueprint', new Schema({
+  capability: { type: Schema.Types.Mixed }
 }));
 
-module.exports = {
-    Blueprint, FacilityBlueprint
-  };
+const AircraftBlueprint = Blueprint.discriminator('AircraftBlueprint', new Schema({
+  stats: { type: Schema.Types.Mixed }
+}));
+
+const SquadBlueprint = Blueprint.discriminator('SquadBlueprint', new Schema({
+  unknown: { type: Schema.Types.Mixed } //Squads don't exist yet...
+}));
+
+const UpgradeBlueprint = Blueprint.discriminator('UpgradeBlueprint', new Schema({
+  unknown: { type: Schema.Types.Mixed } //Upgrades don't exist yet...
+}));
+
+module.exports = { Blueprint, FacilityBlueprint, AircraftBlueprint, SquadBlueprint, UpgradeBlueprint };
--- server/wts/construction/blueprintLoad.js
+++ server/wts/construction/blueprintLoad.js
@@ -1,25 +1,51 @@
 const fs = require('fs')
-const config = require('config');
+const bpLoadDebugger = require('debug')('app:blueprints');
+const facilityData = JSON.parse(fs.readFileSync(require.resolve('../json/facilities/facilities.json')));
+const aircraftData = JSON.parse();
+const squadData = JSON.parse();
+const upgradeData = JSON.parse();
+const blueprintsData = [...facilityData, ...aircraftData, ...squadData, ...upgradeData];
 
-const file = fs.readFileSync(require.resolve(config.get('initPathWTS') + 'json/facilities/facilities.json'));
-const facilData = JSON.parse(file);
-
-const BlueprintFacility = require('../../models/gov/blueprints');
-
-facilityDebugger = require('debug')('app:facility');
+// Import Blueprint discriminators for all buildables
+// Facility | Aircraft | Spacecraft | Squad | Upgrade
+const { Blueprint, FacilityBlueprint, AircraftBlueprint, SquadBlueprint, UpgradeBlueprint } = require('../../models/gov/blueprints');
 
 // Load function to load all facilitys
-async function loadFacilitys () {
-    let count = 0;
+async function loadBlueprints () {
+    let total = 0;
+    let facilityCount = 0;
+    let aircraftCount = 0;
+    let squadCount = 0;
+    let upgradeCount = 0;
 
-    await facilData.forEach(facil => {
-        facilityDebugger(facil);
-        facilitys[count] = new Facil(facil);
-        count++;
-    });
-
-    return `${count} Facility Templates available in WTS...`
+    for (let bp of blueprintsData) {
+        bpLoadDebugger(bp);
+        let validLoad = true;
+        switch (bp.type) {
+            case ('facility'):
+                bp = await new FacilityBlueprint(bp)
+                facilityCount++
+                break;
+            case ('aircraft'):
+                bp = await new AircraftBlueprint(bp)
+                aircraftCount++
+                break;
+            case ('squad'):
+                bp = await new SquadBlueprint(bp)
+                squadCount++
+                break;
+            case ('upgrade'):
+                bp = await new UpgradeBlueprint(bp)
+                upgradeCount++
+                break;
+            default:
+                bpLoadDebugger('Not a valud blueprint');
+                validLoad = false;
+        }
+        if (validLoad) total++
+    }
+    return `${total} Bluprints available in WTS | ${facilityCount} Faclities | ${aircraftCount} Aircrafts | ${squadCount} Squads | ${upgradeCount} upgrades`
 };
 
 
-module.exports = { loadFacilitys,  };
+module.exports = { loadBlueprints };
