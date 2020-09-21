e5af5110039f9e2d359e315c6af78c1201cf925a
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-09-21 03:31:07 -0700

    MOVED - Blueprint model

--- server/wts/construction/blueprintLoad.js
+++ server/wts/construction/blueprintLoad.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 const bpLoadDebugger = require('debug')('app:blueprints');
 const facilityData = JSON.parse(fs.readFileSync(require.resolve('../json/facilities/facilities.json')));
 const aircraftData = JSON.parse(fs.readFileSync(require.resolve('../json/aircraft_bp.json')));
@@ -8,47 +8,47 @@ const blueprintsData = [...facilityData, ...aircraftData, ...facilityUpgrade];//
 
 // Import Blueprint discriminators for all buildables
 // Facility | Aircraft | Spacecraft | Squad | Upgrade
-const { Blueprint, FacilityBlueprint, AircraftBlueprint, SquadBlueprint, UpgradeBlueprint } = require('../../models/gov/blueprint');
+const { Blueprint, FacilityBlueprint, AircraftBlueprint, SquadBlueprint, UpgradeBlueprint } = require('../../models/blueprint');
 
 // Load function to load all facilitys
 async function loadBlueprints () {
-    let total = 0;
-    let facilityCount = 0;
-    let aircraftCount = 0;
-    let squadCount = 0;
-    let upgradeCount = 0;
+	let total = 0;
+	let facilityCount = 0;
+	let aircraftCount = 0;
+	let squadCount = 0;
+	let upgradeCount = 0;
 
-    await Blueprint.deleteMany();
-    for (let bp of blueprintsData) {
-        bpLoadDebugger(bp);
-        let validLoad = true;
-        switch (bp.buildModel) {
-            case ('facility'):
-                bp = new FacilityBlueprint(bp);
-                bp = await bp.save();
-                facilityCount++
-                break;
-            case ('aircraft'):
-                bp = await new AircraftBlueprint(bp);
-                bp = await bp.save();
-                aircraftCount++
-                break;
-            case ('squad'):
-                bp = await new SquadBlueprint(bp)
-                squadCount++
-                break;
-            case ('upgrade'):
-                bp = await new UpgradeBlueprint(bp);
-                bp = await bp.save();
-                upgradeCount++
-                break;
-            default:
-                bpLoadDebugger('Not a valud blueprint');
-                validLoad = false;
-        }
-        if (validLoad) total++
-    }
-    return `${total} Bluprints available in WTS | ${facilityCount} Faclities | ${aircraftCount} Aircrafts | ${squadCount} Squads | ${upgradeCount} upgrades`
+	await Blueprint.deleteMany();
+	for (let bp of blueprintsData) {
+		bpLoadDebugger(bp);
+		let validLoad = true;
+		switch (bp.buildModel) {
+		case ('facility'):
+			bp = new FacilityBlueprint(bp);
+			bp = await bp.save();
+			facilityCount++;
+			break;
+		case ('aircraft'):
+			bp = await new AircraftBlueprint(bp);
+			bp = await bp.save();
+			aircraftCount++;
+			break;
+		case ('squad'):
+			bp = await new SquadBlueprint(bp);
+			squadCount++;
+			break;
+		case ('upgrade'):
+			bp = await new UpgradeBlueprint(bp);
+			bp = await bp.save();
+			upgradeCount++;
+			break;
+		default:
+			bpLoadDebugger('Not a valud blueprint');
+			validLoad = false;
+		}
+		if (validLoad) total++;
+	}
+	return `${total} Bluprints available in WTS | ${facilityCount} Faclities | ${aircraftCount} Aircrafts | ${squadCount} Squads | ${upgradeCount} upgrades`;
 };
 
 //
--- server/wts/construction/construction.js
+++ server/wts/construction/construction.js
@@ -2,78 +2,77 @@ const { Aircraft } = require('../../models/ops/aircraft');
 const { Facility } = require('../../models/facility');
 const { Squad } = require('../../models/ops/squad');
 const { Military } = require('../../models/ops/military/military');
-const { FacilityBlueprint, AircraftBlueprint, UpgradeBlueprint, Blueprint } = require('../../models/gov/blueprint');
+const { FacilityBlueprint, AircraftBlueprint, UpgradeBlueprint, Blueprint } = require('../../models/blueprint');
 
 const { loadBlueprints } = require('../../wts/construction/blueprintLoad');
 const { Upgrade } = require('../../models/upgrade');
 
-//construction function for building Squads, Military(?), and Aircraft
-async function newUnit (name, facility, type, team,){
-    await loadBlueprints(); //this can me taken out when you implement the init loadBlueprints
-       
-    switch(type){
-        case "Fighter":
-            let blue = await AircraftBlueprint.findOne({ type: type }); // Find BP by ID in the future.
-            let base = await Facility.findById(facility).populate('site');
+// construction function for building Squads, Military(?), and Aircraft
+async function newUnit (name, facility, type, team) {
+	await loadBlueprints(); // this can me taken out when you implement the init loadBlueprints
 
-            let fighter = new Aircraft({
-                name,
-                team,
-                zone: base.site.zone._id,
-                country: base.site.country._id,
-                site: base.site._id,
-                origin: facility,
-                mission: 'Under Construction',
-                stats: blue.stats,
-                type: blue.type,
-            });
-            
-            fighter.status.ready = false;
-            fighter.status.building = true;
+	switch(type) {
+	case 'Fighter':
+		const blue = await AircraftBlueprint.findOne({ type: type }); // Find BP by ID in the future.
+		const base = await Facility.findById(facility).populate('site');
 
-            fighter = await fighter.save();
+		let fighter = new Aircraft({
+			name,
+			team,
+			zone: base.site.zone._id,
+			country: base.site.country._id,
+			site: base.site._id,
+			origin: facility,
+			mission: 'Under Construction',
+			stats: blue.stats,
+			type: blue.type
+		});
 
-            return fighter;
-        default:
-            return err = `Could not determine what type of unit was wanted for construction...`;
-            
-    }
+		fighter.status.ready = false;
+		fighter.status.building = true;
 
-}
+		fighter = await fighter.save();
 
-//construction function for making a new upgrade
-async function newUpgrade (code, team, facility ){
-    await loadBlueprints(); //this can me taken out when you implement the init loadBlueprints
- 
-    let x = await UpgradeBlueprint.find();
-    let blue = await UpgradeBlueprint.findOne({ code: code});
-    let upgrade = new Upgrade(blue);
-    upgrade.team = team;
-    upgrade.facility = facility;
+		return fighter;
+	default:
+		return err = 'Could not determine what type of unit was wanted for construction...';
 
-    return upgrade;
-}
-
-async function newFacility (name, site, team){
-    await loadBlueprints(); //this can me taken out when you implement the init loadBlueprints
-    try{
-        let blue = await FacilityBlueprint.findOne({ code: "BS-1" }); //findOne({ name: iData.name });
-        if (!blue)
-            return err = "Could not find Facility Blueprint"
-
-        let facility = new Facility(blue);
-
-        facility.name = name;
-        facility.site = site;
-        //facility.code = code;
-        facility.team = team;
-        //facility = await facility.save();
-
-        return facility;
-    }
-    catch(err){
-        return err;
-    }
+	}
 
 }
-module.exports = {newUnit, newUpgrade, newFacility};
+
+// construction function for making a new upgrade
+async function newUpgrade (code, team, facility) {
+	await loadBlueprints(); // this can me taken out when you implement the init loadBlueprints
+
+	const x = await UpgradeBlueprint.find();
+	const blue = await UpgradeBlueprint.findOne({ code: code });
+	const upgrade = new Upgrade(blue);
+	upgrade.team = team;
+	upgrade.facility = facility;
+
+	return upgrade;
+}
+
+async function newFacility (name, site, team) {
+	await loadBlueprints(); // this can me taken out when you implement the init loadBlueprints
+	try{
+		const blue = await FacilityBlueprint.findOne({ code: 'BS-1' }); // findOne({ name: iData.name });
+		if (!blue) {return err = 'Could not find Facility Blueprint';}
+
+		const facility = new Facility(blue);
+
+		facility.name = name;
+		facility.site = site;
+		// facility.code = code;
+		facility.team = team;
+		// facility = await facility.save();
+
+		return facility;
+	}
+	catch(err) {
+		return err;
+	}
+
+}
+module.exports = { newUnit, newUpgrade, newFacility };
