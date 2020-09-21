c7ab01456fd1701c504f125f72944fb1abdca125
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-09-21 03:48:51 -0700

    FIXED - ESlint errors

--- server/models/loggers/logError.js
+++ server/models/loggers/logError.js
@@ -3,24 +3,24 @@ const Schema = mongoose.Schema;
 const Joi = require('joi');
 
 const LogErrorSchema = new Schema({
-  timestamp: { type: Date},
-  level: {type: String},
-  message: {type: String},
-  meta: {
-    message: {type: String},
-    name: {type: String},
-    stack: {type: String}
-  }
-  });
+	timestamp: { type: Date },
+	level: { type: String },
+	message: { type: String },
+	meta: {
+		message: { type: String },
+		name: { type: String },
+		stack: { type: String }
+	}
+});
 
-let LogError = mongoose.model('logError', LogErrorSchema);
+const LogError = mongoose.model('logError', LogErrorSchema);
 
-function validateLogError(logError) {
-  const schema = {
-    message: Joi.string().min(1)
-  };
+function validateLogError (logError) {
+	const schema = {
+		message: Joi.string().min(1)
+	};
 
-  return Joi.validate(logError, schema);
+	return Joi.validate(logError, schema);
 }
 
 module.exports = { LogError, validateLogError };
