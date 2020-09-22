9b2e533229c83668d5938d4fd32258507e50aacb
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-09-21 22:32:53 -0700

    ADDED - Comments to Account model

--- server/models/account.js
+++ server/models/account.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const Joi = require('joi');
+const mongoose = require('mongoose'); // Mongo DB object modeling module
+const Joi = require('joi'); // Schema description & validation module
+const { logger } = require('../middleware/log/winston'); // Loging midddleware
+const nexusError = require('../middleware/util/throwError'); // Costom error handler util
 
-const nexusError = require('../middleware/util/throwError');
-const { Team } = require('./team');
+// Global Constants
+const Schema = mongoose.Schema; // Destructure of Schema
+const { Team } = require('./team'); // Import of Team model [Mongoose]
 
+// Transfer sub-schema
 const TransferSchema = new Schema({
 	to: { type: String },
 	from: { type: String },
@@ -12,6 +15,7 @@ const TransferSchema = new Schema({
 	note: { type: String }
 });
 
+// Account Schema
 const AccountSchema = new Schema({
 	model: { type: String, default: 'Account' },
 	team: { type: Schema.Types.ObjectId, ref: 'Team', require },
@@ -25,7 +29,9 @@ const AccountSchema = new Schema({
 	gameState: []
 });
 
+// validateAccount method
 AccountSchema.methods.validateAccount = async function () {
+	logger.info(`Validating ${this.model.toLowerCase()} ${this.name}...`);
 	const schema = {
 		name: Joi.string().min(2).max(50).required(),
 		code: Joi.string().min(3).max(3).required().uppercase()
@@ -41,6 +47,6 @@ AccountSchema.methods.validateAccount = async function () {
 	if (team.length < 1) nexusError(`No team exists with the ID: ${this.team}`, 400);
 };
 
-const Account = mongoose.model('account', AccountSchema);
+const Account = mongoose.model('account', AccountSchema); // Creation of Account Model
 
 module.exports = { Account };
