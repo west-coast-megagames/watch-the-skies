c6eddc0e72c066459c1e48de9ab3ea867bd189fe
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-09-24 05:25:13 -0700

    MOVED - Home Route from API to Public

--- server/routes/public/home.js
+++ server/routes/public/home.js
@@ -6,8 +6,8 @@ const router = express.Router();
 // @Desc    Welcome
 // @access  Public
 router.get('/', async function (req, res) {
-    routeDebugger('Someone tried to visit the app landing page!');
-    res.send('Welcome to the West Coast Megagames app, you will need to know your endpoints to get anything. Contact John Cleveland for more information!');
+	routeDebugger('Someone tried to visit the app landing page!');
+	res.send('Welcome to the West Coast Megagames app, you will need to know your endpoints to get anything. Contact John Cleveland for more information!');
 });
 
 module.exports = router;
