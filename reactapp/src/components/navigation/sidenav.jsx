7413ba1f4e646437cc0935eff5864e15098ec971
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-10-02 17:22:12 -0700

    FIXED - Side Nav is always on top

--- reactapp/src/components/navigation/sidenav.jsx
+++ reactapp/src/components/navigation/sidenav.jsx
@@ -7,59 +7,59 @@ import ClockControls from '../clockControls';
 const iconStyles = { width: 56, height: 56, lineHeight: '56px', textAlign: 'center' };
 
 class SideNav extends Component {
-    state = {
-        expand: false,
-        active: null
-    }
+	state = {
+		expand: false,
+		active: null
+	}
 
-    handleToggle = () => {
-        this.setState({
-          expand: !this.state.expand
-        });
-    }
+	handleToggle = () => {
+		this.setState({
+			expand: !this.state.expand
+		});
+	}
 
-    handleSelect = (activeKey) => {
-      this.setState({ active: activeKey });
-    }
+	handleSelect = (activeKey) => {
+		this.setState({ active: activeKey });
+	}
 
-    setKey = (key) => {
-      this.setState({ active: key })
-    }
+	setKey = (key) => {
+		this.setState({ active: key })
+	}
 
-    render() {
-        const { expand, active } = this.state;
-        return (
-            <Sidebar
-            style={{ display: 'flex', flexDirection: 'column' }}
-            width={expand ? 200 : 56}
-            collapsible
-          >
-            <Sidenav
-              expanded={expand}
-              defaultOpenKeys={['9']}
-              appearance="subtle"
-              activeKey={active}
-              onSelect={this.handleSelect}
-            >
-              <Sidenav.Body>
-                <Nav>
-                  <Nav.Item eventKey="1" to="/gov" componentClass={NavLink} icon={<Icon icon="bank" />}>Governance</Nav.Item>
-                  <Nav.Item eventKey="2" to="/ops" componentClass={NavLink} icon={<Icon icon="globe2" />}>Operations</Nav.Item>
-                  <Nav.Item eventKey="3" to="/sci" componentClass={NavLink} icon={<Icon icon="flask" />}>Science</Nav.Item>
-                  <Nav.Item eventKey="4" to="/dip" componentClass={NavLink} icon={<Icon icon="handshake-o" />}>Diplomacy</Nav.Item>
-                  <Nav.Item eventKey="6" to="/news" componentClass={NavLink} icon={<Icon icon="newspaper-o" />}>News</Nav.Item>
-                  <Nav.Item eventKey="7" to="/home" componentClass={NavLink} icon={<Icon icon="info-circle" />}>Info</Nav.Item>
-                  <Whisper placement="right" trigger="click" speaker={clock}>
-                    <Nav.Item eventKey="8" icon={<Icon icon="clock-o"/>}>Game Clock</Nav.Item>
-                  </Whisper>
-                  {this.props.user === 'FroBoyX' ? <Nav.Item eventKey="9" to="/control" componentClass={NavLink} icon={<Icon icon="ge" />}>Control</Nav.Item> : null}
-                </Nav>
-              </Sidenav.Body>
-            </Sidenav>
-            <NavToggle login={this.state.login} expand={expand} onChange={this.handleToggle} signOut={this.handleSignout} />
-            </Sidebar>
-        );
-    }
+	render() {
+		const { expand, active } = this.state;
+		return (
+			<Sidebar
+				style={{ display: 'flex', flexDirection: 'column', zIndex: 999 }}
+				width={expand ? 200 : 56}
+				collapsible
+			>
+				<Sidenav
+					expanded={expand}
+					defaultOpenKeys={['9']}
+					appearance="subtle"
+					activeKey={active}
+					onSelect={this.handleSelect}
+				>
+					<Sidenav.Body>
+						<Nav>
+							<Nav.Item eventKey="1" to="/gov" componentClass={NavLink} icon={<Icon icon="bank" />}>Governance</Nav.Item>
+							<Nav.Item eventKey="2" to="/ops" componentClass={NavLink} icon={<Icon icon="globe2" />}>Operations</Nav.Item>
+							<Nav.Item eventKey="3" to="/sci" componentClass={NavLink} icon={<Icon icon="flask" />}>Science</Nav.Item>
+							<Nav.Item eventKey="4" to="/dip" componentClass={NavLink} icon={<Icon icon="handshake-o" />}>Diplomacy</Nav.Item>
+							<Nav.Item eventKey="6" to="/news" componentClass={NavLink} icon={<Icon icon="newspaper-o" />}>News</Nav.Item>
+							<Nav.Item eventKey="7" to="/home" componentClass={NavLink} icon={<Icon icon="info-circle" />}>Info</Nav.Item>
+							<Whisper placement="right" trigger="click" speaker={clock}>
+								<Nav.Item eventKey="8" icon={<Icon icon="clock-o"/>}>Game Clock</Nav.Item>
+							</Whisper>
+							{this.props.user === 'FroBoyX' ? <Nav.Item eventKey="9" to="/control" componentClass={NavLink} icon={<Icon icon="ge" />}>Control</Nav.Item> : null}
+						</Nav>
+					</Sidenav.Body>
+				</Sidenav>
+				<NavToggle login={this.state.login} expand={expand} onChange={this.handleToggle} signOut={this.handleSignout} />
+			</Sidebar>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
