1bb000c95fb1a76144592a5f8d0a5d1de5a4d240
FroBoyX <49675074+FroBoyX@users.noreply.github.com>
2020-10-12 15:44:18 -0700

    Delete config.js

--- reactapp/src/config.js
+++
@@ -1,8 +0,0 @@
-let gameServer = "http://localhost:5000/";
-let mapKey = 'AIzaSyA_d1zJREiLhBphyJNWTQutlZ0yqWpgO3Q'
-
-if (process.env.NODE_ENV === "production") {
-  gameServer = "https://project-nexus-prototype.herokuapp.com/";
-}
-
-export { gameServer, mapKey };
