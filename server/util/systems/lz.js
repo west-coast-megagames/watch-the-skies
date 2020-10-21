906ba1023d76f179ab7d14473597d0bf155e7a8c
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-10-21 08:46:02 -0700

    FIXED - Typo that sent aircraft acrossed the world

--- server/util/systems/lz.js
+++ server/util/systems/lz.js
@@ -4,7 +4,7 @@ const randomCords = (lat, lng) => {
 	let coinToss = dice.rand(2);
 	const randLat = coinToss == 2 ? lat + (dice.rand(200) * 0.002) : lat - (dice.rand(200) * 0.002);
 	coinToss = dice.rand(2);
-	const randLng = coinToss == 2 ? lng + (dice.rand(200) * 0.002) : lat - (dice.rand(200) * 0.002);
+	const randLng = coinToss == 2 ? lng + (dice.rand(200) * 0.002) : lng - (dice.rand(200) * 0.002);
 
 	return { lat: randLat, lng: randLng };
 };
