a9907e5f8c7906166a663ba0979a1d91cffaf472
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-10-27 03:48:36 -0700

    ADDED - process check

--- reactapp/src/config.js
+++ reactapp/src/config.js
@@ -3,6 +3,7 @@ let mapKey = '';
 
 if (process.env.NODE_ENV === "production") {
 	gameServer = 'https://project-nexus-prototype.herokuapp.com/';
+	console.log(process.env)
 	mapKey = process.env.MAP_KEY;
 }
 
