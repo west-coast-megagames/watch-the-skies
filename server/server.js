5e37e80c886156c455665d9b77e4db9a5e9acbf4
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-10-27 00:27:15 -0700

    RAGE EDIT - Console Log hell!

--- server/server.js
+++ server/server.js
@@ -2,16 +2,16 @@ const express = require('express'); // Import of EXPRESS to create routing app
 const http = require('http'); // Import of the NODE HTTP module to create the http server
 const { logger } = require('./middleware/log/winston'); // Import of winston for error logging
 
-logger.info('Starting boot-up for WTS Game server...');
+console.log('Starting boot-up for WTS Game server...');
 
 require('supports-color'); // Allows colors for debug panel
 
 // Start up proceesses
-logger.info('Loading Start-up proceesses...');
+console.log('Loading Start-up proceesses...');
 const app = express(); // Init for express
-logger.info('Express app started...');
+console.log('Express app started...');
 const server = http.createServer(app); // Creation of an HTTP server
-logger.info('HTTP server created...');
+console.log('HTTP server created...');
 require('./middleware/log/logging')(); // Bootup for error handling
 require('./routes/sockets')(server); // Starts websocket
 require('./routes/routes')(app); // Bootup for Express routes
@@ -22,6 +22,6 @@ require('./middleware/production/prod')(app); // Production compression and midd
 app.use(express.static('public'));
 
 const port = process.env.PORT || 5000; // Server entry point - Node Server
-server.listen(port, () => logger.info(`WTS Server started on port ${port}...`));
+server.listen(port, () => console.log(`WTS Server started on port ${port}...`));
 
 module.exports = server; // Export of Server for JEST testing
