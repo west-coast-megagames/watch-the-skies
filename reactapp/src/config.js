2d05e713ddd811edf7b657703df8a2b7562e3f6e
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-10-27 03:14:34 -0700

    ADDED - gameServer hard coded

--- reactapp/src/config.js
+++ reactapp/src/config.js
@@ -2,7 +2,7 @@ let gameServer = 'http://localhost:5000/';
 let mapKey = '';
 
 if (process.env.NODE_ENV === "production") {
-	gameServer = process.env.serverURL;
+	gameServer = 'https://project-nexus-prototype.herokuapp.com/';
 	mapKey = process.env.mapKey;
 }
 
