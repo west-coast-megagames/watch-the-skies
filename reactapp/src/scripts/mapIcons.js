90e90a4cc1ee0b778fe9c1ab70612b2c84f01a85
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-11-07 11:08:14 -0800

    ADDED - Point of Interest Icon

--- reactapp/src/scripts/mapIcons.js
+++ reactapp/src/scripts/mapIcons.js
@@ -5,9 +5,12 @@ const icon = {
 	National: 'https://cdn.discordapp.com/attachments/481529413482315777/774161300020264960/Aircraft_generic.png',
 	Alien: 'https://cdn.discordapp.com/attachments/481529413482315777/774161302259892234/UFO_generic.png',
 	Unknown: 'https://cdn.discordapp.com/attachments/481529413482315777/774161297972527104/unknown_generic.png',
+	PoI: 'https://cdn.discordapp.com/attachments/483851642605928449/774211449462718484/interest_site.png',
+	City: 'https://cdn.discordapp.com/attachments/483851642605928449/774234464548945940/City_site.png'
 }
 
 const getMapIcon = (type) => {
+	if (type === 'Point of Interest') type = 'PoI';
 	return icon[type];
 }
 
