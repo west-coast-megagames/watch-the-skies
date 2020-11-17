ec79e7be38359a22d12eaf2a1adcb0e090a3c683
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-11-16 23:01:37 -0800

    ADDED - Dynamic description options

--- server/wts/intercept/battleDetails.js
+++ server/wts/intercept/battleDetails.js
@@ -1,17 +1,20 @@
 const { rand } = require('../../util/systems/dice');
 
 const IRDS = ['ALPHA', 'BRAVO', 'CHARLIE', 'DELTA', 'ECHO', 'FOXTROT', 'GOLF', 'HOTEL', 'INDIA', 'JULIET', 'KILO', 'LIMA', 'MIKE', 'NOVEMBER', 'OSCAR', 'PAPA', 'QUEBEC', 'ROMEO', 'SIARRA', 'TANGO', 'UNIFORM', 'VICTOR', 'WHISKEY', 'XRAY', 'YANKEE', 'ZULU'];
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456-';
 
 // Attempting an interception
 function engageDesc (unit, country) {
 	const intro = [
 		`${unit.name} has intercepted target craft.`,
+		`${unit.name} reports target sighted`,
 		`${unit.name} locked on to target.`,
 		`${unit.name} confirms target.`
 	];
 	let desc = intro[rand(intro.length - 1)];
 	const outro = [
 		`${desc}`,
+		`${desc} Interception underway.`,
 		`${desc} ${country.name} command has given the order to engage.`,
 		`${desc} ${unit.name} is pressing the attack.`
 	];
@@ -40,15 +43,15 @@ function escortDesc (unit, target) {
 }
 
 // Engaging an escorted aircraft
-function escortEngageDesc (unit, report) {
+function escortEngageDesc (unit) {
 	const intro = [
-		`${report} Target seems to have an escort.`,
-		`${report} Visual on target, target is in a formation.`,
-		`${report} Clear run to the target, prepearing to engage.`
+		' Target seems to have an escort.',
+		' Visual on target, target is in a formation.',
+		' Clear run to the target, prepearing to engage.'
 	];
 	let desc = intro[rand(intro.length - 1)];
 	const outro = [
-		`${desc}`,
+		`${desc} Engaging hostiles.`,
 		`${desc} Escort is breaking off to engage.`,
 		`${desc} Aborting ${unit.mission}, target escort has engaged.`
 	];
@@ -57,4 +60,20 @@ function escortEngageDesc (unit, report) {
 	return desc;
 }
 
-module.exports = { engageDesc, escortDesc, escortEngageDesc };
+function randCode (length) {
+	let code = '';
+	let newChar = '';
+	for (let i = 0; i < length; i++) {
+		if (newChar === '-' || code === '' || i == length - 1) {
+			newChar = characters.charAt(rand(characters.length - 1) - 1);
+		}
+		else {
+			newChar = characters.charAt(rand(characters.length) - 1);
+		}
+		code += newChar;
+	}
+
+	return code;
+}
+
+module.exports = { engageDesc, escortDesc, escortEngageDesc, randCode };
