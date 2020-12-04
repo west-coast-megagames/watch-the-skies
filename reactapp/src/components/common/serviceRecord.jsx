5447e836bc3e8a673c3edb4ea4bf6a80357ae1f0
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-12-03 16:54:18 -0800

    FIXED - Temp fix for service records

--- reactapp/src/components/common/serviceRecord.jsx
+++ reactapp/src/components/common/serviceRecord.jsx
@@ -4,25 +4,25 @@ import { Timeline, Panel } from 'rsuite'
 import { useSelector } from 'react-redux'
 
 const ServiceRecord = (props) => {
-    let logs = useSelector(state => state.entities.logs.list)
-    logs = logs.filter(log => props.owner._id === log.unit || props.owner._id === log.aircraft);
-    logs.sort((a, b) => new Date(b.date) - new Date(a.date))
+    let reports = useSelector(state => state.entities.reports.list)
+    reports = reports.filter(log => props.owner._id === log.unit || props.owner._id === log.aircraft);
+    reports.sort((a, b) => new Date(b.date) - new Date(a.date))
 
-    let s = logs.length !== 1 ? 's' : '';
+    let s = reports.length !== 1 ? 's' : '';
 
     return(
-    <Panel header={`Service Record - ${logs.length} Report${s}`} collapsible bordered>
-      {logs.length === 0 && <p>No service record available...</p>}
-      {logs.length >= 1 &&
+    <Panel header={`Service Record - ${reports.length} Report${s}`} collapsible bordered>
+      {reports.length === 0 && <p>No service record available...</p>}
+      {reports.length >= 1 &&
       <Timeline style={{marginLeft: '16px'}}>
-        {logs.map(log => {
-            if (log.type === 'Interception') return (<InterceptLog key={log._id} log={log} />)
-            if (log.type === 'Transaction') return (<TransactionLog key={log._id} log={log} />)
-            if (log.type === 'Research') return (<ResearchLog key={log._id} log={log} />)
-            if (log.type === 'Deploy') return (<DeployLog key={log._id} log={log} />)
-						if (log.type === 'Aircraft Repair') return (<RepairLog key={log._id} log={log} />)
-						if (log.type === 'Recon') return (<ReconLog key={log._id} log={log} />);
-						if (log.type === 'Failure') return (<FailedLog key={log._id} log={log} />);
+        {reports.map(report => {
+            if (report.type === 'Interception') return (<InterceptLog key={report._id} report={report} />)
+            if (report.type === 'Transaction') return (<TransactionLog key={report._id} report={report} />)
+            if (report.type === 'Research') return (<ResearchLog key={report._id} report={report} />)
+            if (report.type === 'Deploy') return (<DeployLog key={report._id} report={report} />)
+						if (report.type === 'Aircraft Repair') return (<RepairLog key={report._id} report={report} />)
+						if (report.type === 'Recon') return (<ReconLog key={report._id} report={report} />);
+						if (report.type === 'Failure') return (<FailedLog key={report._id} report={report} />);
         })}
     </Timeline>}
     </Panel>
