bb0728ae562faca894b43789c29bb662d2ad8a3f
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-01-29 06:26:36 -0800

    UPDATE - Formatting

--- server/middleware/events/events.js
+++ server/middleware/events/events.js
@@ -1,4 +1,4 @@
-const events = require('events')
+const events = require('events');
 
 class NexusEvent extends events {}
 
--- server/util/systems/socketServer.js
+++ server/util/systems/socketServer.js
@@ -1,37 +1,37 @@
 class SocketServer {
-    constructor() {
-        this.connections = [];
-        this.saveUser = this.saveUser.bind(this);
-        this.delClient = this.delClient.bind(this);
-        this.getUsers = this.getUsers.bind(this);
-    }
-    saveUser (data, client) {
-        client.user = data.user;
-        client.team = data.team;
-        let index = this.connections.findIndex(el => el.id === client.id)
-        this.connections[index] = client;
-        //console.log(this.connections);
-        return
-    }
+	constructor () {
+		this.connections = [];
+		this.saveUser = this.saveUser.bind(this);
+		this.delClient = this.delClient.bind(this);
+		this.getUsers = this.getUsers.bind(this);
+	}
+	saveUser (data, client) {
+		client.user = data.user;
+		client.team = data.team;
+		const index = this.connections.findIndex(el => el.id === client.id);
+		this.connections[index] = client;
+		// console.log(this.connections);
+		return;
+	}
 
-    delClient (client) {
-        let index = this.connections.findIndex(el => el.id === client.id);
-        console.log(this.connections[index].name)
-        this.connections.splice(index, 1);
-        return
-    }
-    getUsers() {
-        let users = []
-        for (let client of this.connections) {
-            let user = {
-                name: client.user,
-                team: client.team,
-                id: client.id
-            };
-            users.push(user);
-        }
-        return users
-    }
-  }
+	delClient (client) {
+		const index = this.connections.findIndex(el => el.id === client.id);
+		console.log(this.connections[index].name);
+		this.connections.splice(index, 1);
+		return;
+	}
+	getUsers () {
+		const users = [];
+		for (const client of this.connections) {
+			const user = {
+				name: client.user,
+				team: client.team,
+				id: client.id
+			};
+			users.push(user);
+		}
+		return users;
+	}
+}
 
-  module.exports = SocketServer;
+module.exports = SocketServer;
