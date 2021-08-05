6dea8a9c4b17c1027315365a4a133bb15e8d7c72
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-08-04 19:38:38 -0700

    ADDED - Transfer & Schedule socket routes

--- server/routes/socket/transactions.js
+++ server/routes/socket/transactions.js
@@ -3,26 +3,18 @@ const { Account } = require('../../models/account'); // Account Model
 
 const bankDebugging = require('debug')('sockets:Transactions'); // Debug console log
 
-const masterClock = require('../../wts/gameClock/gameClock');
-
 module.exports = async function (client, req) {
 	logger.info(`${client.username} has made a ${req.action} request!`);
+	let { from } = req.data
+	let account = await Account.findOne({ _id: from });
 	let message;
 	switch(req.action) {
 	case('transfer'):
-		let { from, to } = req.data
-		let withdrawalAccount = await Account.findOne({ _id: from });
-		bankDebugging(`${withdrawalAccount.owner} has initiated a transfer!`);
-		withdrawalAccount.withdrawal(req.data);
-		
-		let depositAccount = await Account.findOne({ _id: to });
-		dipositAccount.deposit(req.data);
-
-		bankDebugging(`${withdrawalAccount.owner}s transfer completed!`);
-		return;
+		bankDebugging(`${account.owner} has initiated a transfer!`);
+		account.transfer(req.data);
 		break;
 	case('schedule'):
-		let
+		account.schedule(transaction);
 	default:
 		message = `No ${req.action} is in the ${req.route} route.`;
 		client.emit('alert', { type: 'error', message });
