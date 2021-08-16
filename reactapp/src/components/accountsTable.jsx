f2da5e7eb06ced69a4bb4d524d5e10539a3f1e54
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-08-16 14:02:05 -0700

    ADDED - Resource type filtering

--- reactapp/src/components/accountsTable.jsx
+++ reactapp/src/components/accountsTable.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Table, Loader } from 'rsuite';
+import { Table, Loader, ButtonGroup, Button, ButtonToolbar } from 'rsuite';
 const { Column, HeaderCell, Cell } = Table;
 
 const AccountsTable = (props) => {
@@ -10,13 +10,14 @@ const AccountsTable = (props) => {
 		const accounts = []
 		let newTotal = 0;
 		for (let account of props.accounts) {
-			let resource = account.resources.find(el => el.type === 'Megabucks');
-			accounts.push({name: account.name, balance: resource.balance})
-			newTotal += resource.balance;
+			let type = account.resources.find(el => el.type === props.resource);
+			let balance = type ? type.balance : 0
+			accounts.push({name: account.name, balance })
+			newTotal += balance;
 		}
 		setAccounts(accounts);
 		setTotal(newTotal);
-	}, [props.accounts]);
+	}, [props.accounts, props.resource]);
 
 
 	if (accounts.length < 1) {
@@ -26,22 +27,24 @@ const AccountsTable = (props) => {
 	};
 
 	return (
-		<Table
-			width={260}
-			hover={true}
-			autoHeight={true}
-			data={accounts}
-			defaultSortType="desc"
-		>
-			<Column sortable={true} width={160}>
-					<HeaderCell>Account</HeaderCell>
-					<Cell dataKey="name" />
-			</Column>
-			<Column align="center" width={80}>
-					<HeaderCell title="Total" summary={total}>Total ({total})</HeaderCell>
-					<Cell dataKey="balance" />
-			</Column>
-		</Table>
+		<div>
+			<Table
+				width={260}
+				hover={true}
+				autoHeight={true}
+				data={accounts}
+				defaultSortType="desc"
+			>
+				<Column sortable={true} width={140}>
+						<HeaderCell>Account</HeaderCell>
+						<Cell dataKey="name" />
+				</Column>
+				<Column align="center" width={100}>
+						<HeaderCell title="Total" summary={total}>Total ({total})</HeaderCell>
+						<Cell dataKey="balance" />
+				</Column>
+			</Table>
+		</div>
 	);
 };
 
