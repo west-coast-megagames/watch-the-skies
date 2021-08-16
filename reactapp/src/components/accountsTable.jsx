4fec5d4b9a738bf323aa991d7ebaa244b536ce32
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-08-16 11:48:29 -0700

    FIXED - Account Table

--- reactapp/src/components/accountsTable.jsx
+++ reactapp/src/components/accountsTable.jsx
@@ -1,43 +1,48 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Table, Loader } from 'rsuite';
 const { Column, HeaderCell, Cell } = Table;
-class AccountsTable extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: this.props.accounts
-		};
-	}
 
-	render() {
-		let total = 0;
-		this.state.data.forEach(account => { total += account.balance; });
+const AccountsTable = (props) => {
+	const [accounts, setAccounts] = React.useState([]);
+	const [total, setTotal] = React.useState(0);
 
-		if (!this.props.accounts) {
-			return(
-				<Loader backdrop content="No accounts Loaded..." vertical />
-				);
-		};
+	useEffect(() => {
+		const accounts = []
+		let newTotal = 0;
+		for (let account of props.accounts) {
+			let resource = account.resources.find(el => el.type === 'Megabucks');
+			accounts.push({name: account.name, balance: resource.balance})
+			newTotal += resource.balance;
+		}
+		setAccounts(accounts);
+		setTotal(newTotal);
+	}, [props.accounts]);
 
-		return (
-			<Table
-				width={180}
-				hover={true}
-				autoHeight={true}
-				data={this.props.accounts}
-				defaultSortType="desc"
-			>
-				<Column sortable={true} width={100}>
-						<HeaderCell>Account</HeaderCell>
-						<Cell dataKey="name" />
-				</Column>
-				<Column align="center" width={60}>
-						<HeaderCell title="Total" summary={total}>Total</HeaderCell>
-						<Cell dataKey="balance" />
-				</Column>
-			</Table>
-		);
-	}
-}
+
+	if (accounts.length < 1) {
+		return(
+			<Loader backdrop content="No accounts Loaded..." vertical />
+			);
+	};
+
+	return (
+		<Table
+			width={260}
+			hover={true}
+			autoHeight={true}
+			data={accounts}
+			defaultSortType="desc"
+		>
+			<Column sortable={true} width={160}>
+					<HeaderCell>Account</HeaderCell>
+					<Cell dataKey="name" />
+			</Column>
+			<Column align="center" width={80}>
+					<HeaderCell title="Total" summary={total}>Total {total}</HeaderCell>
+					<Cell dataKey="balance" />
+			</Column>
+		</Table>
+	);
+};
 
 export default AccountsTable;
