332046fc61a1b487cf0c9fa36c0b98e878471da8
SMcMann <smcmann42@gmail.com>
2021-08-26 13:49:15 -0700

    intel api

--- server/models/intel.js
+++ server/models/intel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose'); // Mongo DB object modeling module
 const clock = require('../wts/gameClock/gameClock');
-const { d6 } = require('../util/systems/dice');
 
 // Global Constants
 const Schema = mongoose.Schema; // Destructure of Schema
