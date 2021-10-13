99e478e43b27b252f169eaf43f4764004029e69a
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-10-12 19:25:27 -0700

    FIXED - Consistancy?

--- server/middleware/util/arrayCalls.js
+++ server/middleware/util/arrayCalls.js
@@ -1,6 +1,5 @@
 // remove arrVal from arr ... nothing returned
 async function clearArrayValue(arr, arrVal) {
-
 	if (arr === undefined) throw new Error('Array not defined in array/clearValue');
 	if (arrVal === undefined) throw new Error('Array Element Value not defined in array/clearValue');
 	for (var i = 0; i < arr.length; i++) {
@@ -12,7 +11,6 @@ async function clearArrayValue(arr, arrVal) {
 
 // add arrVal to arr if not already there  ... nothing returned
 async function addArrayValue(arr, arrVal) {
-
 	if (arr === undefined) throw new Error('Array not defined in array/addArrayValue');
 	if (arrVal === undefined) throw new Error('Array Element Value not defined in array/addArrayValue');
 	if (!arr.some(el => el === arrVal)) {
@@ -29,7 +27,6 @@ async function inArray(arr, arrVal) {
 
 // return index of arrVal in arr
 async function getArrayIndex(arr, arrVal) {
-	
 	if (arr === undefined) throw new Error('Array not defined in array/getArrayIndex');
 	if (arrVal === undefined) throw new Error('Array Element Value not defined in array/getArrayIndex');
 	for (let i = 0; i < arr.length; i++) {
