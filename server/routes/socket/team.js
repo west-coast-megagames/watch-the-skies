c4a59af8b2e37c97a776f4070aaf789dce1bb80a
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-10-12 19:00:05 -0700

    ADDED - Register route

---
+++ server/routes/socket/team.js
@@ -0,0 +1,29 @@
+const nexusEvent = require('../../middleware/events/events');
+const { logger } = require('../../middleware/log/winston'); // middleware/error.js which is running [npm] winston for error handling
+const masterClock = require('../../wts/gameClock/gameClock');
+
+const { Team } = require('../../models/team');
+
+module.exports = async function (client, req) {
+	try {
+		logger.info(`${client.username} has made a ${req.action} request in the ${req.route} route!`);
+		let message;
+		switch(req.action) {
+		case('register'):
+				try { 
+					let team = await Team.findById(req.data.team);
+					team = await team.assignUser(req.date.user);
+					client.emit('alert', { type: 'success', message: `${team.shortnName} got a new user (${team.users.length} members on team!).` });
+				} catch (error) {
+					client.emit('alert', { type: 'error', message: error.message ? error.message : error });
+				}
+			break;
+			default:
+				message = `No ${req.action} is in the ${req.route} route.`;
+				throw new Error(message);
+			}
+	} catch (error) {
+		client.emit('alert', { type: 'error', message: error.message ? error.message : error });
+		console.log(error);
+	}
+};
