284ac6bc78e21e4c14ffdc7244804ad682ed17dd
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-10-24 18:40:34 -0700

    ADDED - Transfer action

--- server/routes/socket/aircraft.js
+++ server/routes/socket/aircraft.js
@@ -1,9 +1,24 @@
 const { logger } = require('../../middleware/log/winston'); // middleware/error.js which is running [npm] winston for error handling
+const { Aircraft } = require('../../models/aircraft');
 
 module.exports = async function (client, req) {
 	try {
 		logger.info(`${client.username} has made a ${req.action} request in the ${req.route} route!`);
 		switch(req.action) {
+		case('transfer'):
+		// Deploy action expects UNITS & DESTINATION
+		for (const _id of req.data.units) {
+			try {
+				let unit = await Aircraft.findById(_id);
+				await unit.populateMe();
+				let message = await unit.transfer(req.data.destination);
+				client.emit('alert', { type: 'success', message });
+			} catch (error) {
+				logger.error(`SOCKET-${req.route} [${req.action}]: ${error.message}`, { meta: error.stack });
+				client.emit('alert', { type: 'error', message: error.message ? error.message : error });
+			}
+		}
+		break;
 		default: {
 			let message = `No ${req.action} is in the ${req.route} route.`;
 			throw new Error(message);
