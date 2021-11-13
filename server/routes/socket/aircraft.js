45779072e4879a28655158f8882f114e3720a419
SMcMann <smcmann42@gmail.com>
2021-11-12 20:25:39 -0800

    squish

--- server/routes/socket/aircraft.js
+++ server/routes/socket/aircraft.js
@@ -32,9 +32,9 @@ module.exports = async function (client, req) {
 			}
 			break;
 		case('mission'):
-			for (let unit in req.data.aircrafts) {
+			for (const id of req.data.aircrafts) {
 
-				let aircraft = await Aircraft.findById(unit).populate('upgrades').populate('site').populate('origin').populate('team');
+				let aircraft = await Aircraft.findById(id).populate('upgrades').populate('site').populate('origin').populate('team');
 
 				if (req.data.mission === 'interception' || req.data.mission === 'escort' || req.data.mission === 'recon aircraft') {
 					target = await Aircraft.findById(req.data.target).populate('upgrades').populate('site');
